431b4eb81ee1fe1b93a843cb191dba4b8fc8c9ba
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-01-12 14:10:46 +0200

    suositaan keskellä olevia

--- robo-nolla/ai.py
+++ robo-nolla/ai.py
@@ -78,30 +78,44 @@ class Ai:
         self.vaaka()
         self.pysty()
         self.diagonaali_oikealle()
-        self.loytyi_paikka = False
-        s = sorted(self.kaikki_suorat, key=lambda x: x[0], reverse = True)         
-        #print(s)
+        self.loytyi_paikka = False                                     
 
+        kp = self.leveys // 2    # self.kaikki_suorat = [perakkaisia, alkupiste, loppupiste]
+        s = sorted(self.kaikki_suorat, key=lambda x: (-x[0], abs(x[2][0] - kp) + abs(x[2][1] - kp)))  # 1) eniten perkäkkäisiä
+                                                                                                     # 2) keskeisin loppup. sij. ryhmän sisällä 
+        #sama_maara_perakkaisia = [piste for  piste in s if piste[0] == s[0][0] ]       
+        print(s)
+ 
         for i in range(len(s)):
-            kokelas = s[i]   
-            #print(kokelas)
+            kokelas = s[i]  
+            print("kokelas", kokelas)
             if not kokelas == []:
-                y = kokelas[2][0]   # loppupiste
-                x = kokelas[2][1]
-                y2 = kokelas[1][0]   # alkupiste
-                x2 = kokelas[1][1]
+                print("abs", abs(kokelas[2][0] - kp) + abs(kokelas[2][1] - kp) ,  abs(kokelas[1][0] - kp) + abs(kokelas[1][1] - kp))
+                if abs(kokelas[2][0] - kp) + abs(kokelas[2][1] - kp) < abs(kokelas[1][0] - kp) + abs(kokelas[1][1] - kp):
+                    y = kokelas[2][0]   # loppupiste
+                    x = kokelas[2][1]
+                    y2 = kokelas[1][0]   # alkupiste
+                    x2 = kokelas[1][1]
+                else:
+                    y = kokelas[1][0]   # alkupiste
+                    x = kokelas[1][1]
+                    y2 = kokelas[2][0]   # loppupiste
+                    x2 = kokelas[2][1]
 
-                if y >= 0 and y < self.korkeus and x >= 0 and x < self.korkeus:
+                if y >= 0 and y < self.korkeus and x >= 0 and x < self.korkeus:    # ensin loppupiste (yleensä)
                     if self.kartta[y][x] == 0:
+                        print("x, y")
                         self.kartta[y][x] = 1     # huom!  [y][x]
                         self.loytyi_paikka = True
                         break
-                
-                if y2 >= 0 and y2 < self.korkeus and x2 >= 0 and x2 < self.korkeus:
+                    
+                if y2 >= 0 and y2 < self.korkeus and x2 >= 0 and x2 < self.korkeus:  # sitten alkupiste (yleensä)
                     if self.kartta[y2][x2] == 0:
+                        print("x2, y2")
                         self.kartta[y2][x2] = 1 
                         self.loytyi_paikka = True
-                        break                
+                        break    
+
         self.kaikki_suorat = []
 
         if not self.loytyi_paikka:
@@ -109,6 +123,14 @@ class Ai:
                 # random (ei reunat):
                 y = random.randint(1, self.korkeus -2)
                 x = random.randint(1, self.leveys -2)
+                if self.kartta[y][x] == 0:
+                    self.kartta[y][x] = 1 
+                    break
+        if not self.loytyi_paikka:
+            while True:
+                # random reunat:
+                y = random.randint(0, self.korkeus -1)
+                x = random.randint(0, self.leveys -1)
                 if self.kartta[y][x] == 0:
                     self.kartta[y][x] = 1 
                     break
@@ -127,6 +149,8 @@ class Ai:
                     ykkosluokkaa = True
             return ykkosluokkaa
     """
+
+    """ TODO:  nykyisin saattaa blokata ylh. alas / muun muodostuvan suoran
     def mahtuuko_5_vaaka(self, perakkaisia, alkupiste, loppupiste):   
             x_raja = 4 - perakkaisia
             #print(alkupiste[1], x_raja, self.leveys - loppupiste[1])   
@@ -134,6 +158,7 @@ class Ai:
             if alkupiste[1] < x_raja or self.leveys - loppupiste[1] < x_raja :
                 return False
             return True     
+    """
 
     def vaaka(self):
         perakkaisia = 0  
@@ -150,12 +175,12 @@ class Ai:
                     alku_x = alkupiste[1]
                     loppu_x = loppupiste[1]
                     if alku_x >= 0 or loppu_x < self.korkeus :                    # oli: and
-                        if self.mahtuuko_5_vaaka(perakkaisia, alkupiste, loppupiste) or self.mahtuuko_5_pysty(perakkaisia, alkupiste, loppupiste):   
-                            self.kaikki_suorat.append([perakkaisia, alkupiste, loppupiste])
+                        #if self.mahtuuko_5_vaaka(perakkaisia, alkupiste, loppupiste) or self.mahtuuko_5_pysty(perakkaisia, alkupiste, loppupiste):   
+                        self.kaikki_suorat.append([perakkaisia, alkupiste, loppupiste])
                 else:
                     perakkaisia = 0 
 
-
+    """   TODO:  nykyisin saattaa blokata ylh. alas muodostuvan suoran
     def mahtuuko_5_pysty(self, perakkaisia, alkupiste, loppupiste):   
             y_raja = 4 - perakkaisia
             #print(alkupiste[0], y_raja, self.leveys - loppupiste[0])   
@@ -163,6 +188,7 @@ class Ai:
             if alkupiste[0] < y_raja or self.leveys - loppupiste[0] < y_raja :
                 return False
             return True
+    """
 
     def pysty(self):
         perakkaisia = 0  
@@ -179,11 +205,12 @@ class Ai:
                     alku_y = alkupiste[0]
                     loppu_y = loppupiste[0]
                     if alku_y >= 0 or loppu_y < self.korkeus :                  # oli: and
-                        if self.mahtuuko_5_pysty(perakkaisia, alkupiste, loppupiste) or self.mahtuuko_5_vaaka(perakkaisia, alkupiste, loppupiste):  
-                            self.kaikki_suorat.append([perakkaisia, alkupiste, loppupiste])
+                    #    if self.mahtuuko_5_pysty(perakkaisia, alkupiste, loppupiste) or self.mahtuuko_5_vaaka(perakkaisia, alkupiste, loppupiste):  
+                        self.kaikki_suorat.append([perakkaisia, alkupiste, loppupiste])
                 else:
                     perakkaisia = 0 
 
+    """ TODO:  nykyisin saattaa blokata toisen suoran
     def mahtuuko_5_d_oik(self, perakkaisia, alkupiste, loppupiste):   
             y_raja = 4 - perakkaisia
             x_raja = 4 - perakkaisia
@@ -192,6 +219,7 @@ class Ai:
             if (alkupiste[0] < y_raja or self.korkeus - loppupiste[0] < y_raja) and (alkupiste[1] < x_raja or self.leveys - loppupiste[1] < x_raja) :
                 return False
             return True
+    """
 
     def diagonaali_oikealle(self):
         perakkaisia = 0  
@@ -210,16 +238,16 @@ class Ai:
                         if x_jatko + 1 < len(self.kartta) -1:                           
                             if self.kartta[y_jatko+1][x_jatko+1] == 1:         
                                 perakkaisia += 1  
-                                loppupiste = (y + 1 , x + 1)
+                                loppupiste = (y_jatko + 2 , x_jatko + 2)
                                 # lisätään potentiaaliset uudet sijainnit, jos kartalla:
                                 alku_x = alkupiste[1]
                                 loppu_x = loppupiste[1]
                                 alku_y = alkupiste[0]
                                 loppu_y = loppupiste[0]
                                 if (alku_x >= 0 or loppu_x < self.korkeus) and (alku_y >= 0 or loppu_y < self.korkeus) :
-                                    if self.mahtuuko_5_d_oik(perakkaisia, alkupiste, loppupiste) :   
-                                        print(perakkaisia, alkupiste, loppupiste)
-                                        self.kaikki_suorat.append([perakkaisia, alkupiste, loppupiste])                             
+                                    #if self.mahtuuko_5_d_oik(perakkaisia, alkupiste, loppupiste) :   
+                                    print(perakkaisia, alkupiste, loppupiste)
+                                    self.kaikki_suorat.append([perakkaisia, alkupiste, loppupiste])                             
                                 
                             y_jatko += 1
                             x_jatko += 1 
