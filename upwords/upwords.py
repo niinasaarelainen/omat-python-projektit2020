0db4a1b7d7258bf5172f973b8131fff96b8bda7b
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-01-29 18:03:45 +0200

    murmeli

--- upwords/upwords.py
+++ upwords/upwords.py
@@ -11,8 +11,8 @@ blockSize = 70 #Set the size of the grid block
 kirjainten_lkm = 10
 kirjainvali = 57
 
-#aakkoset = list("AAAAABDEEEFGHHHIIIIIIIIIJJJKKKKKLLLLLMMMMNNNOOOOOOPPRRRSSSSTTTTUUUUUUUUVVVVVYYYÄÄÄÄÄÄÖÖÖÖ")
-aakkoset = list("AAABDEEEFGHHIIIKLM")
+aakkoset = list("AAAAABDEEEFGHHHIIIIIIIIIJJJKKKKKLLLLLMMMMNNNOOOOOOPPRRRSSSSTTTTUUUUUUUUVVVVVYYYÄÄÄÄÄÄÖÖÖÖ")
+#aakkoset = list("AAABDEEEFGHHIIIKLM")
 pel1_7 = []
 pel2_7 = []
 ruudukko = []
@@ -28,7 +28,21 @@ def alusta_ruudukko():
     for rivi in range(kirjainten_lkm):
         ruudukko.append([])
         for sarake in range(kirjainten_lkm):
-            ruudukko[rivi].append("")       
+            ruudukko[rivi].append("")     
+    ruudukko[4][2] = "M"  
+    ruudukko[4][3] = "U"  
+    ruudukko[4][4] = "R"  
+    ruudukko[4][5] = "M"  
+    ruudukko[4][6] = "E"  
+    ruudukko[4][7] = "L"
+    ruudukko[4][8] = "I"    
+    kerrokset[2, 4] = 1
+    kerrokset[3, 4] = 1
+    kerrokset[4, 4] = 1
+    kerrokset[5, 4] = 1
+    kerrokset[6, 4] = 1
+    kerrokset[7, 4] = 1
+    kerrokset[8, 4] = 1
 
 
 def uusi_nappi():    
@@ -108,26 +122,32 @@ def minne_kirjain(x, y, kirjain):
 
 def tutki_mouse(x, y, vuoro, kirjain):
     kirjain_ind = -1
+
     if vuoro % 4 == 1 and y < blockSize:
         kirjain_ind = mika_kirjain(x)
-        kirjain = sorted(pel1_7)[kirjain_ind]             
+        kirjain = sorted(pel1_7)[kirjain_ind]   
+        vuoro += 1          
   
     elif vuoro % 4 == 2 and y >= blockSize and y <= WINDOW_HEIGHT - blockSize:
         if minne_kirjain(x, y, kirjain):
             pel1_7.remove(kirjain)
         if len(aakkoset) > 0:
             pel1_7.append(uusi_nappi())
+        vuoro -= 1   # oletus että lisätään useampi kuin 1 kirjain
 
     elif vuoro % 4 == 3 and y > WINDOW_HEIGHT - blockSize:
         kirjain_ind = mika_kirjain(x)
         kirjain = sorted(pel2_7)[kirjain_ind]
+        vuoro += 1 
 
     elif vuoro % 4 == 0 and y >= blockSize and y <= WINDOW_HEIGHT - blockSize:
         if minne_kirjain(x, y, kirjain):
             pel2_7.remove(kirjain)
         if len(aakkoset) > 0:
             pel2_7.append(uusi_nappi())
-    return kirjain, kirjain_ind
+        vuoro -= 1 
+
+    return kirjain, kirjain_ind, vuoro
 
 
 def main():
@@ -147,8 +167,14 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:   
                 x = event.pos[0]
                 y = event.pos[1]  
-                kirjain, kirjain_ind = tutki_mouse(x, y, vuoro, kirjain)               
-                vuoro += 1
+                
+                if  (15 <= x <= 45  and  30 <= y <= 50) or (15 <= x <= 45  and  WINDOW_HEIGHT - 40 <= y <= WINDOW_HEIGHT - 10) :
+                    vuoro += 2
+                    print(vuoro)
+                else:
+                    kirjain, kirjain_ind, vuoro_uusi = tutki_mouse(x, y, vuoro, kirjain)     
+                    vuoro = vuoro_uusi  
+                    print(vuoro)
         
         if kirjain_ind > 0 and (vuoro % 4 == 1 or vuoro % 4 == 2):   #   -1 = ei tarvitse neliöidä
             rect = pygame.Rect(278 + kirjain_ind * kirjainvali , 19, blockSize // 2, blockSize // 2)
