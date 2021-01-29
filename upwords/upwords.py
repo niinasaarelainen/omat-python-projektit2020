fa6ab1e6981a076d063fc97955002c73b4014db0
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-01-29 09:18:47 +0200

    pelaajan kirjaimet ok

--- upwords/upwords.py
+++ upwords/upwords.py
@@ -13,14 +13,19 @@ blockSize = 70 #Set the size of the grid block
 aakkoset = list("AAABDEEEFGHHIIIKLM")
 pel1_7 = []
 pel2_7 = []
-
+ruudukko = [][]
 
 
 def jaa_napit_aloitus():
     for i in range(7):
         pel1_7.append(uusi_nappi())
-    for i in range(7):
         pel2_7.append(uusi_nappi())
+    
+def alusta_ruudukko():
+    for rivi in range(10):
+        for sarake in range(10):
+            ruudukko[rivi][sarake] = ""
+        
 
 
 def uusi_nappi():    
@@ -39,35 +44,50 @@ def drawGrid():
 def tekstit():
     fontti = pygame.font.SysFont("FreeMono", 32)
     pel1_str = "  "
-    pel1_str = pel1_str.join(pel1_7) 
+    pel1_str = pel1_str.join(sorted(pel1_7)) 
     teksti = fontti.render(f"Pelaaja 1:   {pel1_str}", True, WHITE)
     SCREEN.blit(teksti, (40, 20))
 
     pel2_str = "  "
-    pel2_str = pel2_str.join(pel2_7) 
+    pel2_str = pel2_str.join(sorted(pel2_7)) 
     teksti = fontti.render(f"Pelaaja 2:   {pel2_str}", True, WHITE)
     SCREEN.blit(teksti, (40, WINDOW_HEIGHT - 50))
     
 
 def mika_kirjain(x):
-    # 297, 353, 412, 466,  525, 580, 638    kirjainten keskipisteet
+    # 297, 353, 412, 466, 525, 580, 638    kirjainten keskipisteet
     kirjainvali = 57
     indeksi = 0
     for i in range(7):
         if x > 297 - (kirjainvali // 2) + i * kirjainvali:
             indeksi = i
     return indeksi
+
+def minne_kirjain(x, y, kirjain):
+    x_indeksi = 0
+    y_indeksi = 0 
+    for i in range(10):
+        if x > i * blockSize :
+            x_indeksi = i
+        if y > blockSize + i * blockSize :   # ylhäällä muuta tekstiä yhden blockSize:n verran
+            y_indeksi = i
+    fontti = pygame.font.SysFont("FreeMono", 32)
+    teksti = fontti.render(kirjain, True, WHITE)
+    SCREEN.blit(teksti, (x_indeksi *blockSize , y_indeksi *blockSize))
     
 
 def tutki_mouse(x, y, vuoro):
+    kirjain_ind = ""
+    kirjain = ""
     if vuoro % 4 == 1 and y < blockSize:
-        kirjain = mika_kirjain(x)
+        kirjain_ind = mika_kirjain(x)
+        kirjain = sorted(pel1_7)[kirjain_ind]
         print(kirjain)
     elif vuoro % 4 == 2 and y >= blockSize and y <= WINDOW_HEIGHT - blockSize:
-        print("1 pelitila")
+        minne_kirjain(x, y, kirjain)
     elif vuoro % 4 == 3 and y > WINDOW_HEIGHT - blockSize:
-        kirjain = mika_kirjain(x)
-        print(kirjain)
+        kirjain_ind = mika_kirjain(x)
+        print(sorted(pel2_7)[kirjain_ind])
     elif vuoro % 4 == 0 and y >= blockSize and y <= WINDOW_HEIGHT - blockSize:
         print("2 pelitila")  
 
@@ -88,12 +108,6 @@ def main():
                 y = event.pos[1]  
                 tutki_mouse(x, y, vuoro)
                 vuoro += 1
-                """
-                if vuoro % 2 == 1:   
-                    vuoro = 2
-                else:
-                    vuoro = 1     
-                """  
             CLOCK.tick(1000)   
             pygame.display.update()
             tekstit()
@@ -113,5 +127,6 @@ def main():
         
         
 
-jaa_napit_aloitus()     
+jaa_napit_aloitus()    
+alusta_ruudukko() 
 main()
