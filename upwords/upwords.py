80a222b340cc8614534968c11b697b67070f2afe
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-01-29 15:09:40 +0200

    peruslogiikka ok2

--- upwords/upwords.py
+++ upwords/upwords.py
@@ -9,12 +9,14 @@ SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CLOCK = pygame.time.Clock()
 blockSize = 70 #Set the size of the grid block
 kirjainten_lkm = 10
+kirjainvali = 57
 
 #aakkoset = list("AAAAABDEEEFGHHHIIIIIIIIIJJJKKKKKLLLLLMMMMNNNOOOOOOPPRRRSSSSTTTTUUUUUUUUVVVVVYYYÄÄÄÄÄÄÖÖÖÖ")
 aakkoset = list("AAABDEEEFGHHIIIKLM")
 pel1_7 = []
 pel2_7 = []
 ruudukko = []
+kerrokset = {}
 
 
 def jaa_napit_aloitus():
@@ -40,9 +42,16 @@ def drawGrid():
     for x in range(kirjainten_lkm):
         for y in range(1,kirjainten_lkm +1):                 
             rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
-            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)   # 1 = vain reunat = mustaa keskellä
 
-def tekstit():
+def tekstit(vuoro):
+   
+    if vuoro % 4 == 1 or vuoro % 4 == 2:        
+        rect = pygame.Rect(15, 30, blockSize // 4, blockSize // 4)
+    else:
+        rect = pygame.Rect(15, WINDOW_HEIGHT - 40, blockSize // 4, blockSize // 4)
+    pygame.draw.rect(SCREEN, WHITE, rect, 0)    # 0 = kokovalkoinen
+
     fontti = pygame.font.SysFont("FreeMono", 32)
     pel1_str = "  "
     pel1_str = pel1_str.join(sorted(pel1_7)) 
@@ -55,17 +64,22 @@ def tekstit():
     SCREEN.blit(teksti, (40, WINDOW_HEIGHT - 50))
 
     fontti = pygame.font.SysFont("FreeMono", 50)
+    fontti_pieni = pygame.font.SysFont("FreeMono", 24)
     for rivi in range(kirjainten_lkm):
-        for sarake in range(kirjainten_lkm):
+        for sarake in range(kirjainten_lkm):            
             kirjain = ruudukko[rivi][sarake]
             if not kirjain == "":
                 kirjain = fontti.render(kirjain, True, WHITE)
-                SCREEN.blit(kirjain, ((sarake + 0.27) *blockSize , (rivi + 1.17) *blockSize))   
+                SCREEN.blit(kirjain, ((sarake + 0.27) *blockSize , (rivi + 1.17) *blockSize))      
+                kerros_lkm = kerrokset[sarake, rivi]    
+                if kerros_lkm > 1:
+                    kerros = fontti_pieni.render(str(kerros_lkm), True, WHITE)
+                    SCREEN.blit(kerros, (sarake * blockSize , (rivi + 1) * blockSize))    
+
     
 
 def mika_kirjain(x):
     # 297, 353, 412, 466, 525, 580, 638    kirjainten keskipisteet
-    kirjainvali = 57
     indeksi = 0
     for i in range(7):
         if x > 297 - (kirjainvali // 2) + i * kirjainvali:
@@ -81,41 +95,51 @@ def minne_kirjain(x, y, kirjain):
             x_indeksi = i
         if y > blockSize + i * blockSize :   # ylhäällä muuta tekstiä yhden blockSize:n verran
             y_indeksi = i
-    ruudukko[y_indeksi][x_indeksi] = kirjain
-    print(y_indeksi, x_indeksi, kirjain)
-    print(ruudukko)
+    if (x_indeksi, y_indeksi) not in kerrokset:
+        kerrokset[x_indeksi, y_indeksi] = 1        
+        ruudukko[y_indeksi][x_indeksi] = kirjain
+    elif kerrokset[x_indeksi, y_indeksi] == 5:
+        return False
+    else:
+        kerrokset[x_indeksi, y_indeksi] += 1        
+        ruudukko[y_indeksi][x_indeksi] = kirjain
+    return True
     
 
 def tutki_mouse(x, y, vuoro, kirjain):
-    kirjain_ind = ""
+    kirjain_ind = -1
     if vuoro % 4 == 1 and y < blockSize:
         kirjain_ind = mika_kirjain(x)
-        kirjain = sorted(pel1_7)[kirjain_ind]       
+        kirjain = sorted(pel1_7)[kirjain_ind]             
+  
     elif vuoro % 4 == 2 and y >= blockSize and y <= WINDOW_HEIGHT - blockSize:
-        minne_kirjain(x, y, kirjain)
-        pel1_7.remove(kirjain)
+        if minne_kirjain(x, y, kirjain):
+            pel1_7.remove(kirjain)
         if len(aakkoset) > 0:
             pel1_7.append(uusi_nappi())
 
     elif vuoro % 4 == 3 and y > WINDOW_HEIGHT - blockSize:
         kirjain_ind = mika_kirjain(x)
         kirjain = sorted(pel2_7)[kirjain_ind]
+
     elif vuoro % 4 == 0 and y >= blockSize and y <= WINDOW_HEIGHT - blockSize:
-        minne_kirjain(x, y, kirjain)
-        pel2_7.remove(kirjain)
+        if minne_kirjain(x, y, kirjain):
+            pel2_7.remove(kirjain)
         if len(aakkoset) > 0:
             pel2_7.append(uusi_nappi())
-    return kirjain
+    return kirjain, kirjain_ind
 
 
 def main():
     pygame.init()    
-    tekstit()
+    
     vuoro = 1
     kirjain = ""
+    kirjain_ind = -1
 
     while len(pel1_7) > 0 and len(pel2_7) > 0:        
         SCREEN.fill(BLACK)
+        tekstit(vuoro)
         drawGrid()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -123,11 +147,17 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:   
                 x = event.pos[0]
                 y = event.pos[1]  
-                kirjain = tutki_mouse(x, y, vuoro, kirjain)
+                kirjain, kirjain_ind = tutki_mouse(x, y, vuoro, kirjain)               
                 vuoro += 1
-            CLOCK.tick(1000)   
-            tekstit()
-            pygame.display.flip()
+        
+        if kirjain_ind > 0 and (vuoro % 4 == 1 or vuoro % 4 == 2):   #   -1 = ei tarvitse neliöidä
+            rect = pygame.Rect(278 + kirjain_ind * kirjainvali , 19, blockSize // 2, blockSize // 2)
+            pygame.draw.rect(SCREEN, WHITE, rect, 2)
+        elif kirjain_ind > 0 and (vuoro % 4 == 3 or vuoro % 4 == 0):
+            rect = pygame.Rect(278 + kirjain_ind * kirjainvali , WINDOW_HEIGHT - 50, blockSize // 2, blockSize // 2)
+            pygame.draw.rect(SCREEN, WHITE, rect, 2)
+        CLOCK.tick(1000)   
+        pygame.display.flip()
             
         
     print("napit lopussa")
