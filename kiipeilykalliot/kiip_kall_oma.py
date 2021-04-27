0da27ad52df8a9e4ee83b906880fd9f331403b3b
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-04-27 15:43:47 +0300

    jarjesta-->etsi samassa funktiossa

--- kiipeilykalliot/kiip_kall_oma.py
+++ kiipeilykalliot/kiip_kall_oma.py
@@ -21,79 +21,24 @@ def openfile():
             reittilista = []        
     kallion_nimi = ""
     
-def muodosta_kaikki_reitit():
+def muodosta_kaikki_reitit():   # häviää tieto mihin kallioon liittyvät
     kaikki_reitit = []
     for kallio in kalliot.values():
         for reitti in kallio.reitit:
             kaikki_reitit.append(reitti)
     return kaikki_reitit
 
-"""  EI TOIMI !!
-def jarjesta_yhdella_attribuutilla_kalliosta(kallion_nimi, nro):
-    return sorted(reitti.attribuutit[nro], key=lambda reitti: kallion_nimi, reverse = True)    """
-
- 
 
 def etsi_reitti_hakusanalla(hakusana):
     vastaukset = []
     for kallio in kalliot.values():      # HUOM!   ei pelkkä kalliot !!!!!!!!!!!!
         for reitti in kallio.reitit:
-            if hakusana in reitti.nimi:
+            if hakusana in reitti.sanakirja["nimi"]:
                 vastaukset.append(reitti)
     return vastaukset
 
 
-def etsi_tietyn_greidin_reitit(kalliot:list, grade):
-    # tähän tulee yhdet sulkuparit liikaa
-    #vastaus_lista = [kallio.reitit_graden_mukaan(grade) for kallio in kalliot if kallio.reitit_graden_mukaan(grade) != []]
-    
-    for kallio in kalliot.values():        
-        vastaus_lista = kallio.reitit_graden_mukaan(grade)
-        for kallion_nimi, reitti in vastaus_lista:
-           print(kallion_nimi, ": " ,  reitti)
-           
-    #return sorted(kalliot, key=lambda kallio: kallio.reitit_graden_mukaan(grade))   
-    # tämä ei sovelias, koska ei haluata palauttaa kaikkia kallioita: ei välttis ole tiettyä greidiä
-
-def kiivetyt_greidin_mukaan(kalliot:list):
-    vastaus_lista = []
-    for kallio in kalliot.values():        
-        vastaus_lista.append(kallio.kiivetyt())
-    vastaus_lista = reduce(lambda x,y: x+y,vastaus_lista)    # flatten = [1,2,3],[4,5,6], [7] --> [1, 2, 3, 4, 5, 6, 7]
-    #print(vastaus_lista)                # (self.nimi, reitti)
-    return sorted(vastaus_lista, key=lambda kallio: kallio[1].grade)  
-
-
-
-def kiivetyt_aikajarjestyksessa(kalliot:list):   
-    vastaus_lista = []
-    for kallio in kalliot.values():        
-        vastaus_lista.append(kallio.kiivetyt())
-    vastaus_lista = reduce(lambda x,y: x+y,vastaus_lista)    # flatten = [1,2,3],[4,5,6], [7] --> [1, 2, 3, 4, 5, 6, 7]
-    #print(vastaus_lista)                # (self.nimi, reitti)
-    return sorted(vastaus_lista, key=lambda kallio: kallio[1].tikkauspvm)  
-
-
-
-def kiipeamattomat_anna_greidi_saa_tikit(kalliot:list, greidi):
-    vastaus_lista = []
-    for kallio in kalliot.values():        
-        vastaus_lista.append(kallio.kiipeamattomat())
-    vastaus_lista = reduce(lambda x,y: x+y,vastaus_lista)    # flatten = [1,2,3],[4,5,6], [7] --> [1, 2, 3, 4, 5, 6, 7]
-    #print(vastaus_lista)                # (self.nimi, reitti)
-    oikeagreidiset = [kallio for kallio in vastaus_lista if kallio[1].grade == greidi]
-    return sorted(oikeagreidiset, key=lambda kallio: kallio[1].ticks, reverse = True)   
-
-
-def reittien_maaran_mukaan(kalliot:list):
-    return sorted(kalliot.values(), key=lambda kallio: kallio.reitteja())   
-
-def vaikeimman_reitin_mukaan(kalliot:list):
-    #return list(reversed(vaikeuden_mukaan[kallio for kallio.vaikein_reitti() in kalliot])
-    return sorted(kalliot.values(), key=lambda kallio: kallio.vaikein_reitti().grade, reverse = True)   
-
-
-
+######  J Ä R J E S T Ä  ###########
 def jarjesta_reitit_yhden_attribuutin_mukaan(minka_mukaan):
     def yhden_mukaan(reitti):
         return reitti.sanakirja[minka_mukaan]
@@ -104,6 +49,10 @@ def jarjesta_reitit_kahden_attribuutin_mukaan(minka_mukaan1, minka_mukaan2):
         return reitti.sanakirja[minka_mukaan1], reitti.sanakirja[minka_mukaan2]
     return sorted(kaikki_reitit, key=kahden_mukaan)   
 
+def reittien_maaran_mukaan(kalliot:list):     # turha ?!?!?!?
+    return sorted(kalliot.values(), key=lambda kallio: kallio.reitteja())   
+
+######  E T S I  ###########
 def etsi_reitit_yhden_attribuutin_mukaan(mika_attribuutti, mita_etsitaan):
     reitit = [reitti for reitti in kaikki_reitit if reitti.sanakirja[mika_attribuutti] == mita_etsitaan]        
     return reitit
@@ -112,12 +61,18 @@ def etsi_reitit_kahden_attribuutin_mukaan(mika_attribuutti1, mita_etsitaan1, mik
     reitit = [reitti for reitti in kaikki_reitit if reitti.sanakirja[mika_attribuutti1] == mita_etsitaan1 and reitti.sanakirja[mika_attribuutti2] == mita_etsitaan2]        
     return reitit
 
+######  E T S I  --> J Ä R J E S T Ä ###########
+def etsi_sitten_jarjesta_reitit(mika_attribuutti1, mita_etsitaan1, mika_attribuutti2):
+    reitit = [reitti for reitti in kaikki_reitit if reitti.sanakirja[mika_attribuutti1] == mita_etsitaan1]        
+    def yhden_mukaan(reitti):
+        return reitti.sanakirja[mika_attribuutti2]
+    return sorted(reitit, key=yhden_mukaan)  
+
 
 
 if __name__ == "__main__":
 
     kalliot = {}    
-
     openfile()
     kaikki_reitit = muodosta_kaikki_reitit()
     print("     kaikki_reitit", kaikki_reitit)
@@ -126,14 +81,9 @@ if __name__ == "__main__":
     vastaukset = etsi_reitti_hakusanalla("ei")
     monesko = 1
     for reitti in vastaukset:
-        print(f"{monesko}. {reitti.nimi}")
+        print(reitti.sanakirja["nimi"])
+        #print(f"{monesko}. {reitti.sanakirja["nimi"]}")
         monesko += 1
-
-    print("vaikein_reitti:",  kalliot["Olhava"].vaikein_reitti())
-    for kallio in reittien_maaran_mukaan(kalliot):
-        print(kallio)
-
-    print("vaikein_reitti:", kalliot["Olhava"].vaikein_reitti())
 
     # !! metodi luokassa Kiipeilykallio:
     print("\njarjesta_reitit_yhden_attribuutin_mukaan  NIMI  ")
@@ -181,57 +131,6 @@ if __name__ == "__main__":
         print(f"{reitti}")
 
 
-    """
-     # WANHAT:
-
-    k2 = Kiipeilykallio("Nummi")
-    k2.lisaa_reitti(Kiipeilyreitti("Syncro", 14, "8C+"))
-    k2.lisaa_reitti(Kiipeilyreitti("PidempiSyncro", 15, "8C+"))
-
-        
-    print("vaikein_reitti:", nalkkila.vaikein_reitti())
-
-    print(k1)
-    print(nalkkila.nimi, nalkkila.reitteja())
-    print(nalkkila.vaikein_reitti())
-    kalliot = [k1, k2, nalkkila]
-    print()
-    for kallio in reittien_maaran_mukaan(kalliot):
-        print(kallio)
-
-    print()
-    for kallio in vaikeimman_reitin_mukaan(kalliot):
-        print(kallio)
-
-    print("\netsi_tietyn_greidin_reitit:")
-    etsi_tietyn_greidin_reitit(kalliot, "6A+")
-
-    print("\nkiivetyt_greidin_mukaan:")    
-    for kallion_nimi, reitti in kiivetyt_greidin_mukaan(kalliot):
-        print(kallion_nimi, ": " ,  reitti)
-       
-    print("\nkiipeamättomat_anna_greidi_saa_tikit:")
-    for kallion_nimi, reitti in kiipeamattomat_anna_greidi_saa_tikit(kalliot, "6A+"):
-        print(kallion_nimi, ": " ,  reitti)
-
-    niinanErikoinen.tikkaa()
-    print( niinanErikoinen.tikkausvuosi())
-
-    print("\nkiipeamattomat_anna_greidi_saa_tikit:")
-    for kallion_nimi, reitti in kiipeamattomat_anna_greidi_saa_tikit(kalliot, "6A+"):
-        print(kallion_nimi, ": " ,  reitti)
-
-    print("\nkiipeamattomat @ Nalkkila:")
-    for kallion_nimi, reitti in nalkkila.kiipeamattomat():
+    print("\netsi_sitten_jarjesta_reitit   kiipeämättömät, greidi")
+    for reitti in etsi_sitten_jarjesta_reitit("tick", False, "grade"):
         print(reitti)
-
-    print("\nkiivetyt_aikajarjestyksessa:")
-    for kallion_nimi, reitti in kiivetyt_aikajarjestyksessa(kalliot):
-        print(reitti)
-
-    pvm = datetime.datetime.strptime("2020-06-07", '%Y-%m-%d')
-    niinanErikoinen.tikkauspvm = pvm
-    print("\nkiivetyt_aikajarjestyksessa:")
-    for kallion_nimi, reitti in kiivetyt_aikajarjestyksessa(kalliot):
-        print(reitti, reitti.pvm())
-    """
--- kiipeilykalliot/luokat.py
+++ kiipeilykalliot/luokat.py
@@ -7,23 +7,28 @@ class Kiipeilyreitti:
         self.raakadata = data
         self.sanakirja = {}
         self.kasittele_attribuutit()        
-        self.nimi = self.sanakirja["nimi"]        
-        self.pituus =  int(self.sanakirja["pituus"])
+        self.nimi = self.sanakirja["nimi"]           # nämä ei välttämättömät mutta lyhyempi merkintä kuin sanakirja["x"]
+        self.pituus = self.sanakirja["pituus"]   
         self.grade =  self.sanakirja["grade"]
-        self.ticks =  self.sanakirja["ticks"]  
-        self.tick = self.sanakirja["tick"]   # True/ False
-        self.onkokiivetty()        
+        self.ticks =  int(self.sanakirja["ticks"])         
+        self.type = self.sanakirja["type"]
+        self.tick = self.sanakirja["tick"]   # yes / no
+        self.onkokiivetty()                  # -->  True/ False
         self.tikkauspvm = None   
         self.grade_opinion = None
         self.rating = None
-        self.type = self.sanakirja["type"]
-        self.attribuutit = [self.nimi, self.pituus, self.grade, self.ticks, self.type]         # HUOM !!!!!
-                               # 0           1           2            3           4
+
     
     def kasittele_attribuutit(self):
         for pari in self.raakadata:
             self.sanakirja[pari[0]]= pari[1]
         print(self.sanakirja)
+
+    def onkokiivetty(self):
+        if self.sanakirja["tick"] == "yes":
+            self.sanakirja["tick"] = True
+        else:
+            self.sanakirja["tick"] = False
     
     def __gt__(self, verrokki):
         return self.nimi > verrokki.nimi
@@ -32,17 +37,10 @@ class Kiipeilyreitti:
         self.grade_opinion = grade
 
     def anna_rating (self, rating):
-        self.rating = rating
-     
-
-    def onkokiivetty(self):
-        if self.sanakirja["tick"] == "yes":
-            self.sanakirja["tick"] = True
-        else:
-            self.sanakirja["tick"] = False
+        self.rating = rating    
 
     def tikkaa(self):
-        self.onko_kiivetty = True
+        self.tick = True
         self.tikkauspvm = datetime.datetime.today()
         self.ticks += 1
 
@@ -59,12 +57,12 @@ class Kiipeilyreitti:
     def __str__(self):
         return f"{self.nimi}, pituus {self.pituus} metriä, grade {self.grade}, ticks {self.ticks}"
 
+
 ########################################################################################################
 class Kiipeilykallio:
     def __init__(self, nimi: str):
         self.nimi = nimi
         self.reitit = []
-
 
     def jarjesta_reitit_yhden_attribuutin_mukaan(self, minka_mukaan):
         def yhden_mukaan(reitti):
@@ -80,28 +78,17 @@ class Kiipeilykallio:
         reitit = [(self.nimi, reitti) for reitti in self.reitit if reitti.sanakirja[mika_attribuutti] == mita_etsitaan]        
         return reitit
 
-
     def lisaa_reitti(self, reitti: Kiipeilyreitti):
         self.reitit.append(reitti)
 
     def reitteja(self):
-        return len(self.reitit)
+        return len(self.reitit)  
 
-    
-
-    def kiivetyt(self):
-        return [(self.nimi, reitti) for reitti in self.reitit if reitti.onko_kiivetty == True]        
-         
-    def kiipeamattomat(self):
-        return [(self.nimi, reitti) for reitti in self.reitit if reitti.onko_kiivetty == False]        
-
-    def vaikein_reitti(self):
-        def vaikeuden_mukaan(reitti):
-            return reitti.grade, reitti.pituus     ###  HUOM !!!!  Järjestäminen 2 parametrin mukaan
-        return sorted(self.reitit, key=vaikeuden_mukaan)[-1]
+    def kiivetty_lkm(self):
+        return len([reitti for reitti in self.reitit if reitti.sanakirja["tick"] == True])  
 
     def __str__(self):
-        return f"{self.nimi} {self.reitteja()} reittiä, vaikein {self.vaikein_reitti().grade}({self.vaikein_reitti().nimi})"
+        return f"{self.nimi} {self.reitteja()} reittiä, joista kiivetty {self.kiivetty_lkm()}"
 
 
 ########################################################################################################
