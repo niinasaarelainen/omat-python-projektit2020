19ebdf56772ce3f6d15112911a80f53fd8b9e096
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-07-07 13:07:53 +0300

    sama vai eri: mel.pit 3-6

--- sama_vai_eri_melodia/sama_vai_eri_melodia.py
+++ sama_vai_eri_melodia/sama_vai_eri_melodia.py
@@ -4,7 +4,7 @@ import random, pygame.midi, pygame
 pygame.init()    
 pygame.midi.init()
 kello = pygame.time.Clock()
-naytto = pygame.display.set_mode((911, 220))
+naytto = pygame.display.set_mode((911, 320))
 fontti = pygame.font.SysFont("FreeMono", 42)
 fontti_pieni = pygame.font.SysFont("FreeMono", 25)
 port = pygame.midi.get_default_output_id()
@@ -12,17 +12,19 @@ midi_out = pygame.midi.Output(port, 0)
 
 WHITE = (200, 200, 200)
 BLUE = (10, 97, 97)
-RED = (210, 7, 7)
+GREEN = (7, 210, 7)
 blockSize = 60
 x_aloitus = 100
 y_aloitus = 60
 aania = 5       # ambitus kvintti 
 soittoalue = "zxcvbnm,."
-melodiat = {4:[], 5:[], 6:[]}
+melodiat = {3:[], 4:[], 5:[], 6:[]}
 eka = ""
 toka = ""
 sama = False   
 montako_aanta = 4
+AANTEN_MAX = 6
+AANTEN_MIN = 3
 
 
 
@@ -45,15 +47,17 @@ def alkuohjeet(naytto):
         teksti = fontti_pieni.render(f" Klikkaa ovatko ne sama vai eri", True, WHITE)
         naytto.blit(teksti, (30, 100))   
         teksti = fontti_pieni.render(f" Paina nyt ENTER, kun haluat aloittaa", True, WHITE)
-        naytto.blit(teksti, (30, 155))            
+        naytto.blit(teksti, (30, 175))            
 
         pygame.display.flip()
         kello.tick(40)
 
 def muodosta_melodiat():
     global melodiat
+    melodiat[3].append(["zcv", "zcb", "zvc", "zxx", "zxc", "zzz", "zxv", "zbv", "zzv"])
     melodiat[4].append(["zcvb", "zcbb", "zcvc", "zxvb", "zcxb", "zcbb", "zxbb", "zcbv", "zcvv"])
-    # TODO 5, 6
+    melodiat[5].append(["zcvbv", "zcbbv", "zcvcv", "zxvbv", "zcxbv", "zcbbv", "zxbbv", "zcbvv", "zcvvb", "zcbxv", "zcvzb"])
+    melodiat[6].append(["zcvbvx", "zcbbvx", "zcvcvx", "zxvbvx", "zcxbvx", "zcbbvc", "zxbbvx", "zcbvvc", "zcvvbc", "zcbxvx", "zcvzbx"])
 
 
 def valitse_melodiat(montako_aanta):
@@ -66,7 +70,7 @@ def valitse_melodiat(montako_aanta):
         toka = eka
         sama = True
     else:        
-        monesko_melodia2 = random.randint(0, len(melodiat[montako_aanta]) - 1) 
+        monesko_melodia2 = random.randint(0, len(melodiat[montako_aanta][0]) - 1) 
         while monesko_melodia2 == monesko_melodia:
              monesko_melodia2 = random.randint(0, len(melodiat[montako_aanta][0]) - 1) 
         toka = melodiat[montako_aanta][0][monesko_melodia2]
@@ -100,15 +104,22 @@ def draw_tekstit(pisteet, kysytty):
     pygame.draw.rect(naytto, WHITE, rect, 1)   # 1 = vain reunat = mustaa keskellä
     text = fontti.render(f" ERI ", True, BLUE)  
     naytto.blit(text, (300, y_aloitus + 5))
-    teksti = fontti_pieni.render(f" Sama pari uudestaan = Space ,  Esc = lopeta", True, WHITE)
-    naytto.blit(teksti, (65, 183))    
-    virheita = fontti_pieni.render(f"oikein: {pisteet}/{kysytty}", True, RED)  
-    naytto.blit(virheita, (660, y_aloitus + 5))  
+    
+    virheita = fontti_pieni.render(f"oikein: {pisteet}/{kysytty}", True, GREEN)  
+    naytto.blit(virheita, (660, y_aloitus + 5)) 
+    teksti = fontti_pieni.render(f" Soita sama pari uudestaan = Space ,  Esc = lopeta", True, WHITE)
+    naytto.blit(teksti, (65, 183))     
 
+    text = fontti_pieni.render(f" Melodian pituus: nuolet ylös ja alas (nykyinen {montako_aanta})", True, WHITE)  
+    naytto.blit(text, (65, 250))
 
 def main():    
+    global montako_aanta
     pisteet = 0   
     kysytty = 0
+    naytto.fill((10, 10, 10))     
+    draw_tekstit(pisteet, kysytty)      
+    pygame.display.flip()
     valitse_melodiat(montako_aanta)
     soita_melodiat(montako_aanta)   
 
@@ -125,7 +136,15 @@ def main():
                     pygame.quit()
                 if event.key == pygame.K_SPACE:
                     soita_melodiat(montako_aanta) 
-            if event.type == pygame.MOUSEBUTTONDOWN: 
+                if event.key == pygame.K_UP:
+                    if montako_aanta < AANTEN_MAX:
+                        montako_aanta += 1
+                        valitse_melodiat(montako_aanta)
+                if event.key == pygame.K_DOWN:
+                    if montako_aanta > AANTEN_MIN:
+                        montako_aanta -= 1
+                        valitse_melodiat(montako_aanta)
+            if event.type == pygame.MOUSEBUTTONDOWN:  
                 x = event.pos[0]
                 y = event.pos[1]                 
                 if x > x_aloitus + blockSize*3 :
@@ -139,7 +158,7 @@ def main():
                 naytto.fill((10, 10, 10))     
                 draw_tekstit(pisteet, kysytty)                        
                 pygame.display.flip()
-                
+
                 valitse_melodiat(montako_aanta)
                 soita_melodiat(montako_aanta)
                 
