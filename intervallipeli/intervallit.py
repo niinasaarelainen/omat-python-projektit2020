55ec22e30076a084b87e28849cf5c6a347fea55f
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-07-17 13:39:42 +0300

    intervallit: soundinvaihto

--- intervallipeli/intervallit.py
+++ intervallipeli/intervallit.py
@@ -3,19 +3,23 @@ import random, pygame.midi, pygame
 pygame.init()    
 pygame.midi.init()
 kello = pygame.time.Clock()
-naytto = pygame.display.set_mode((800, 600))
+naytto = pygame.display.set_mode((900, 600))
 fontti = pygame.font.SysFont("FreeMono", 42)
 fontti_pieni = pygame.font.SysFont("FreeMono", 25)
+fontti_medium = pygame.font.SysFont("FreeMono", 33)
 port = pygame.midi.get_default_output_id()
 midi_out = pygame.midi.Output(port, 0)
+arrows = pygame.image.load('arrows.png')
+ARROW_X = 630
+ARROW_Y = 280
 
-WHITE = (200, 200, 200)
+WHITE = (250, 250, 250)
 BLUE = (10, 97, 97)
 RED = (220, 0, 0)
 GREEN = (10, 220, 7)
 blockSize = 80
 x_aloitus = 100
-y_aloitus = 130
+y_aloitus = 100
 ruudukko = []
 intervallit = ["s2", "p3", "s3", "pu4", "y4/vä5", "pu5", "p6", "s6", "p7", "s7"]
 oikea_vastaus = 0
@@ -24,7 +28,7 @@ aani2 = 0
 midi_numbers = {0:60, 1:61, 2:62, 3:63, 4:64, 5:65, 6:66, 7:67, 8:68, 9:69, 10:70, 11:71, 12:72 }    #keski-c - h   
 oikein = -1  # aluksi ei kumpikaan, 0 = väärin, 1 = oikein
 
-sitar = 105
+soundi = 105
 
 
 def alusta_ruudukko():
@@ -40,7 +44,7 @@ def drawGrid():
     for x in range(2):
         for y in range(5):                 
             rect = pygame.Rect(x*blockSize*3 + x_aloitus, y*blockSize + y_aloitus, blockSize*3, blockSize)
-            pygame.draw.rect(naytto, WHITE, rect, 1)   # 1 = vain reunat = mustaa keskellä
+            pygame.draw.rect(naytto, BLUE, rect, 1)   # 1 = vain reunat = mustaa keskellä
             ind = ruudukko[y][x]
             interv = intervallit[ind -2]
             text = fontti.render(interv, True, BLUE)  
@@ -88,19 +92,25 @@ def valitse_aanet():
 
 def soita_intervalli():  
     # peräkkäin
-    midi_play([aani1], sitar, 500)  # notes, instrument, ms
-    midi_play([aani2], sitar, 500) 
+    midi_play([aani1], soundi, 500)  # notes, instrument, ms
+    midi_play([aani2], soundi, 500) 
     # yhtaikaa
-    midi_play([aani1, aani2], sitar, 1300)
+    midi_play([aani1, aani2], soundi, 1300)
    
 
 def ruudun_nollaus(pist, kysytty, x_ind, y_ind):
-    naytto.fill((10, 10, 10))  
-    teksti = fontti_pieni.render(f"Sama intervalli uudestaan = Space ", True, WHITE)
-    naytto.blit(teksti, (100, 40))   
+    naytto.fill(WHITE)  
+    
+    naytto.blit(arrows, (ARROW_X , ARROW_Y))
+    teksti = fontti_pieni.render(f"= Vaihda soundia ", True, BLUE)
+    naytto.blit(teksti, (ARROW_X, 355))   
+    teksti = fontti_medium.render(f"Space =  ", True, BLUE)
+    naytto.blit(teksti, (ARROW_X -3 , 410))   
+    teksti = fontti_pieni.render(f"= Sama uudestaan ", True, BLUE)
+    naytto.blit(teksti, (ARROW_X, 450))  
     drawGrid()   
     pisteet = fontti.render(f" {pist}/{kysytty}", True, BLUE)  
-    naytto.blit(pisteet, (640, 25))  
+    naytto.blit(pisteet, (680, 50))  
     if oikein == 1:
         ruutu_oikein(x_ind, y_ind)
     elif oikein == 0:
@@ -108,10 +118,20 @@ def ruudun_nollaus(pist, kysytty, x_ind, y_ind):
     pygame.display.flip()     
 
 
+def vaihda_soundia(suunta):
+    global soundi
+    soundi = soundi + suunta
+    if soundi == 128:
+        soundi = 0
+    if soundi == -1:
+        soundi = 127    
+    midi_play([0], soundi, 500) 
+
 def mainloop():
     global oikein
     kysytty = 0
     pist = 0  
+    naytto.fill(WHITE)  
     alusta_ruudukko()  
     drawGrid()     
     pygame.display.flip()      
@@ -133,12 +153,23 @@ def mainloop():
                     soita_intervalli()  
                     break
 
-            if event.type == pygame.MOUSEBUTTONDOWN: 
-                kysytty += 1   
+            if event.type == pygame.MOUSEBUTTONDOWN:                 
                 x = event.pos[0]
                 x_ind = mika_grid_indeksi_x(x)
                 y = event.pos[1]   
                 y_ind = mika_grid_indeksi_y(y)
+
+                # nuoli ylös:
+                if x >= ARROW_X and x <= ARROW_X + 35 and y >= ARROW_Y and y <= ARROW_Y + 35:
+                    print("nuoli ylös")
+                    vaihda_soundia(1)
+                    break
+                if x >= ARROW_X + 30 and x <= ARROW_X + 70 and y >= ARROW_Y + 25 and y <= ARROW_Y + 60:
+                    print("nuoli alas")
+                    vaihda_soundia(-1)
+                    break
+
+                #onko intervalli:
                 if x_ind in [0,1] and y_ind >= 0 and y_ind <= 4:
                     if oikea_vastaus == ruudukko[y_ind][x_ind]:
                         oikein = 1  
@@ -146,7 +177,8 @@ def mainloop():
                         valitse_aanet()  
                     else:
                         oikein = 0
-                                  
+
+                kysytty += 1                     
                 ruudun_nollaus(pist, kysytty, x_ind, y_ind)  
                 soita_intervalli()  
         
