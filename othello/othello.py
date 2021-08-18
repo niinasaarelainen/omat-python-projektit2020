b4a3d011b7b114995a851fe22f905b3888fc547b
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-08-18 13:15:27 +0300

    otello: peli läpi?

--- othello/othello.py
+++ othello/othello.py
@@ -10,6 +10,7 @@ class Othello:
         self.korkeus = len(self.kartta)  
         self.leveys = len(self.kartta[0])
         self.skaala = 56
+        self.vaihda_vari = []
 
         self.nayton_korkeus = self.skaala * self.korkeus + REUNAN_KOKO
         self.nayton_leveys = self.skaala * self.leveys + REUNAN_KOKO
@@ -19,20 +20,22 @@ class Othello:
         self.fontti_pieni = pygame.font.SysFont("Arial", 22)
 
         pygame.display.set_caption("Othello")
-        self.silmukka()
-    
+        self.silmukka()    
 
 
     def uusi_peli(self):
         self.kartta = []
+        
         for i in range(3):
             self.kartta.append([0, 0, 0, 0, 0, 0, 0, 0])
         self.kartta.append([0, 0, 0, PUNAINEN, SININEN, 0, 0, 0])
         self.kartta.append([0, 0, 0, SININEN, PUNAINEN, 0, 0, 0])
         for i in range(3):
             self.kartta.append([0, 0, 0, 0, 0, 0, 0, 0])
+        
         self.siirrot = 0
         self.vuorossa = PUNAINEN
+
 
     def silmukka(self):
         while True:
@@ -40,27 +43,71 @@ class Othello:
             self.piirra_naytto()            
 
     def laillinen(self, sarake, rivi):
+
+        print()
+
         #vieressä vastustaja:
-        aloitussarake = max(sarake - 1, 0)
-        aloitusrivi = max(rivi - 1, 0)
+        aloitussarake = sarake - 1
+        aloitusrivi = rivi - 1
         vastustaja = PUNAINEN
         if self.vuorossa == PUNAINEN:
             vastustaja = SININEN
-        laillinen = False
+        laillinen_vastustaja = False
         vastustajien_sijainnit = []
-        for x in range(3):
+        for x in range(3):   # TODO ylin rivi !! ei saa tehdä 3 riviä alasp.
             for y in range(3): 
-                if self.kartta[min(aloitusrivi, len(self.kartta) - 1)][min(aloitussarake, len(self.kartta) - 1)] == vastustaja:
-                    laillinen = True
+                if  0 <= aloitusrivi < len(self.kartta) and 0 <= aloitussarake < len(self.kartta) and self.kartta[min(aloitusrivi, len(self.kartta) - 1)][min(aloitussarake, len(self.kartta) - 1)] == vastustaja:
+                    laillinen_vastustaja = True
+                    print(aloitusrivi, aloitussarake)
                     vastustajien_sijainnit.append((min(aloitusrivi, len(self.kartta) - 1), min(aloitussarake, len(self.kartta) - 1)))
                 aloitusrivi += 1
-                print("aloitusrivi:", aloitusrivi, "aloitussarake", aloitussarake)
             aloitussarake += 1
             aloitusrivi = max(rivi - 1, 0)
 
         # oma väri tulee vastaan
-        print(vastustajien_sijainnit)
-        return laillinen
+          # ei tiedä nappulan tutkintahetkellä onko siirto laillinen
+        laillinen_oma_tulee_vastaan = False
+        for y, x in vastustajien_sijainnit:    
+            vaihda_vari_temp = [] 
+            suunta_x =  x - sarake
+            
+            if  suunta_x > 0 :
+                aloitussarake = max(sarake + suunta_x, 0)
+            elif suunta_x == 0:
+                aloitussarake = sarake
+            else:
+                aloitussarake = min(sarake + suunta_x, len(self.kartta) - 1)
+
+            suunta_y =  y - rivi   
+            if  suunta_y > 0 :
+                aloitusrivi = max(rivi + suunta_y, 0)
+            elif suunta_y == 0:
+                aloitusrivi = rivi
+            else:
+                aloitusrivi = min(rivi + suunta_y, len(self.kartta) - 1)
+
+            """
+            print("vastustaja: y", y, "x", x) 
+            print("pelaaja: rivi", rivi, "sarake", sarake)
+            print("suunta_y", suunta_y, "suunta_x", suunta_x)       """                     
+            
+            for x in range(7):
+                while  0 <= aloitusrivi <= len(self.kartta) - 1 and 0 <= aloitussarake <= len(self.kartta) - 1:
+                    #print("aloitusrivi:", aloitusrivi, "aloitussarake", aloitussarake)
+                    if self.kartta[aloitusrivi][aloitussarake] == self.vuorossa:
+                        laillinen_oma_tulee_vastaan = True
+                        for y, x in vaihda_vari_temp:
+                            self.vaihda_vari.append((y, x))
+                            #print("self.vaihda_vari.append((y, x))", y, x)   
+                        break                     
+                        # return laillinen_vastustaja and laillinen_oma_tulee_vastaan
+                    else: 
+                        vaihda_vari_temp.append((aloitusrivi,aloitussarake))
+                    aloitusrivi += suunta_y
+                    aloitussarake += suunta_x
+
+        return laillinen_vastustaja and laillinen_oma_tulee_vastaan
+
 
     def tutki_tapahtumat(self):
         for tapahtuma in pygame.event.get():
@@ -71,11 +118,13 @@ class Othello:
                 self.rivi  = y // self.skaala
                 if self.kartta[self.rivi][self.sarake] == 0 and self.laillinen(self.sarake, self.rivi):
                     if self.vuorossa == PUNAINEN:
-                        self.kartta[self.rivi][self.sarake] = PUNAINEN   # ["tyhja", PUNAINEN, SININEN]
+                        self.kartta[self.rivi][self.sarake] = PUNAINEN   # ["tyhja", PUNAINEN, SININEN]  
+                        self.kaanna()     # tämän voi tehdä vasta kun kääntämisyritys on todettu lailliseksi                 
                         self.vuorossa = SININEN
                     else:
                         self.kartta[self.rivi][self.sarake] = SININEN
-                        self.vuorossa = PUNAINEN
+                        self.kaanna()  
+                        self.vuorossa = PUNAINEN            
 
             elif tapahtuma.type == pygame.KEYDOWN:
                 if tapahtuma.key == pygame.K_F2:
@@ -83,16 +132,35 @@ class Othello:
 
             if tapahtuma.type == pygame.QUIT:
                 exit()
+
+        if self.peli_lapiko():
+            print("peli_lapi")
+            self.peli_lapi()
+
+
+    def peli_lapiko(self):
+        for rivi in self.kartta:
+            if 0 in rivi:
+                return False
+        return True
+
+
+    def kaanna(self):
+        for y, x in self.vaihda_vari:
+            self.kartta[y][x] = self.vuorossa
+        self.vaihda_vari = []
    
 
     def piirra_naytto(self):
-        self.naytto.blit(lauta, (0,0))
+        self.naytto.blit(lauta, (0,0))        
 
         for y in range(self.korkeus):
             for x in range(self.leveys):
                 vari = self.kartta[y][x]
                 if vari == SININEN or vari == PUNAINEN:
                     pygame.draw.circle(self.naytto, vari[0], (x*self.skaala + P_KOKO + REUNAN_KOKO, y*self.skaala + P_KOKO + REUNAN_KOKO), P_KOKO)  
+
+        
 
         teksti = self.fontti_pieni.render(f"Vuorossa: {self.vuorossa[1]}", True, self.vuorossa[0])
         self.naytto.blit(teksti, (33, self.nayton_korkeus + 20))
@@ -115,96 +183,23 @@ class Othello:
 
     def peli_lapi(self):
 
-        def onko_vaaka(pelaaja):
-            perakkaisia = 0
-            alkupiste = []              
-            for y in range(self.korkeus):
-                for x in range(self.leveys):
-                    if self.kartta[y][x] == pelaaja:
-                        perakkaisia += 1
-                        if perakkaisia == 1:
-                            alkupiste.append((x, y))
-                        if perakkaisia == 4:
-                            self.alkupiste = alkupiste[0]
-                            self.loppupiste = (x, y)                       
-                            return True
-                    else:
-                        perakkaisia = 0   
-                        alkupiste = []   
-            return False    
+        self.naytto.blit(lauta, (0,0))        
 
-        def onko_pysty(pelaaja):
-            perakkaisia = 0
-            alkupiste = []  
-            for y in range(self.korkeus):
-                for x in range(self.leveys):
-                    if self.kartta[x][y] == pelaaja:
-                        perakkaisia += 1
-                        if perakkaisia == 1:
-                            alkupiste.append((y, x))
-                        if perakkaisia == 4:
-                            self.alkupiste = alkupiste[0]
-                            self.loppupiste = (y, x) 
-                            return True
-                    else:
-                        perakkaisia = 0    
-                        alkupiste = []  
-            return False    
+        for rivi in self.kartta:
+            for x in range(self.leveys):
+                vari = self.kartta[y][x]
+                if vari == SININEN or vari == PUNAINEN:
+                    pygame.draw.circle(self.naytto, vari[0], (x*self.skaala + P_KOKO + REUNAN_KOKO, y*self.skaala + P_KOKO + REUNAN_KOKO), P_KOKO)  
 
-        def onko_diagonaali(pelaaja):
-            perakkaisia = 0
-            alkupiste = []  
-            x_jatko = 0
-            y_jatko = 0
-            for y in range(self.korkeus - 3):   # 4 sarja vika mahdollisuus alkaa
-                for x in range(self.leveys):
-                    if self.kartta[y][x] == pelaaja:
-                        x_jatko = x 
-                        y_jatko = y      
-                        for i in range(3):   # oik. alas   
-                            if x_jatko < len(self.kartta) -1:                           
-                                if self.kartta[y_jatko+1][x_jatko+1] == pelaaja:         
-                                    perakkaisia += 1
-                                    y_jatko += 1
-                                    x_jatko += 1
-                                else:
-                                    perakkaisia = 0   
-                                    alkupiste = []   
-                                if perakkaisia == 1:
-                                    alkupiste.append((x, y))
-                                if perakkaisia == 3:
-                                    self.alkupiste = alkupiste[0]
-                                    self.loppupiste = (x_jatko, y_jatko)
-                                    return True
-
-                        y_jatko = y   # vas. alas
-                        x_jatko = x
-                        for i in range(3):   
-                            if self.kartta[y_jatko+1][x_jatko-1] == pelaaja:                                
-                                perakkaisia += 1        
-                                y_jatko += 1
-                                x_jatko -= 1
-                            else:
-                                perakkaisia = 0   
-                                alkupiste = []   
-                            if perakkaisia == 1:
-                                alkupiste.append((x, y))
-                            if perakkaisia == 3:
-                                a_x, a_y = alkupiste[0]
-                                self.alkupiste = (a_x + 1, a_y)
-                                self.loppupiste = (x_jatko -1, y_jatko)   
-                                return True
-                    else:
-                        perakkaisia = 0   
-            return False  
-
-        if onko_vaaka(PUNAINEN) or onko_pysty(PUNAINEN) or onko_diagonaali(PUNAINEN):   # ["tyhja", PUNAINEN, SININEN]
-            return True, "punainen voitti"
-
-        if onko_vaaka(SININEN) or onko_pysty(SININEN) or onko_diagonaali(SININEN):   # ["tyhja", PUNAINEN, SININEN]
-            return True, "sininen voitti"
         
-        return False, ""
+
+        teksti = self.fontti_pieni.render(f"Vuorossa: {self.vuorossa[1]}", True, self.vuorossa[0])
+        self.naytto.blit(teksti, (33, self.nayton_korkeus + 20))
+
+        teksti = self.fontti_pieni.render("F2 = uusi peli", True, self.vuorossa[0])
+        self.naytto.blit(teksti, (300, self.nayton_korkeus + 20))
+        
+       
 
 
 if __name__ == "__main__":
