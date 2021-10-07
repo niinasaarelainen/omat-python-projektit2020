33f920022b248172a6095ed5b1f4cbb3d32e504a
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-10-07 09:32:03 +0300

    squid game #1 jarrutus

--- squid games/squid game #1.py
+++ squid games/squid game #1.py
@@ -17,33 +17,52 @@ y = KORKEUS - 100
 oikealle = False
 vasemmalle = False
 vauhti = 0.8
+jarrutan = -1
+vauhdinpudotus = 0
 
 kello = pygame.time.Clock()
 
 while True:
+
+    
+    if jarrutan > 0 :
+        jarrutan -= 1
+        print(vauhti)
+        print(vauhdinpudotus)
+    if jarrutan == 20 :
+        vauhdinpudotus = vauhti - 0.8
+        vauhti -= vauhdinpudotus / 20
+        print("10")
+    elif jarrutan > 0 :
+        vauhti -= vauhdinpudotus / 20
+    elif jarrutan == 0:
+        vauhti = 0.8
+        jarrutan = -1
+        oikealle = False 
+
+
     for tapahtuma in pygame.event.get():
         if tapahtuma.type == pygame.QUIT:
-            pygame.quit()
+            pygame.quit()        
 
         elif tapahtuma.type == pygame.KEYDOWN:
+            stopped = False
             if tapahtuma.key == pygame.K_RIGHT:
                 oikealle = True
-                vauhti += 0.2
-            if tapahtuma.key == pygame.K_LEFT:
-                vasemmalle = True
+                vauhti += 0.2 
 
-        elif tapahtuma.type == pygame.KEYUP:           
-            if tapahtuma.key == pygame.K_RIGHT:
-                oikealle = False
-                vauhti += 0.2
-            if tapahtuma.key == pygame.K_LEFT:
-                vasemmalle = False
-    
+        elif tapahtuma.type == pygame.KEYUP:  
+            jarrutan = 21   # 5 sykliä jarrutusta  
+                
+            
+    keys=pygame.key.get_pressed()   # tämä ei saa olla for tapahtuma in pygame.event.get(): sisällä !!! 
+    if keys[pygame.K_RIGHT]:
+        vauhti += 0.06  
+        
+        
         
     if oikealle and x <= LEVEYS - robon_leveys - vauhti :    #  ei saa mennä reunojen yli !
-            x += vauhti   
-    if vasemmalle and x >= 0 + vauhti:
-            x -= vauhti
+        x += vauhti  
                 
     naytto.fill((0, 0, 0))
     naytto.blit(robo, (x, y))
