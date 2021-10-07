3bb1f632696ffc93a18659391c7c4d48c59063b0
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-10-07 16:53:20 +0300

    squid game #1 sekunnit

--- squid games/squid game #1.py
+++ squid games/squid game #1.py
@@ -16,23 +16,33 @@ robon_korkeus = robo.get_height()
 
 
 def main():
-    global musa_paused, jarrutan, oikealle, x, vauhti, askeleita, danger_time, vauhdinpudotus
-    while True:
+    x = 0
+    time = 0
+    sec = SEKUNTIMAARA
+    textsurface = myfont.render(f"{sec}", True, (100, 30, 30))  
+    oikealle = False
+    vauhti = 0.8
+    jarrutan = -1
+    vauhdinpudotus = 0
+    askeleita = 0
+    musa_paused = False
+    danger_time = DANGER_TIME
 
-        naytto.fill((0, 0, 0))
+    while True:
+        naytto.fill(BLACK)
             
         if musa_paused and danger_time > 0:
             danger_time -= 1
             pygame.draw.rect(naytto, RED, (LEVEYS // 2 - 30, 20, danger_time * 2, 30))
             if danger_time == 0:
-                if jarrutan == -1:
+                if jarrutan > -1:
                     lopetus()
                 print("0")
                 mixer.music.unpause()   
                 musa_paused = False  
                 danger_time = DANGER_TIME 
         else:
-            r = random.randint(0, 210)
+            r = random.randint(0, 210)  # oli 210
             if r == 0:
                 mixer.music.pause()    
                 musa_paused = True 
@@ -84,20 +94,25 @@ def main():
             x += vauhti  
                     
         
-        naytto.blit(robo, (x, y))
+        naytto.blit(robo, (x, y))        
+        time += 1
+        if time % 60 == 0:
+            sec -= 1    
+            if sec == 0:
+                lopetus()
+            textsurface = myfont.render(f"{sec}", True, (100, 30, 30)) 
+        naytto.blit(textsurface, (320, 20))  
+        pygame.display.flip()          
         kello.tick(60)
 
 
 def lopetus():
-    print("lopetus")
+    print("lopetus")    
+    textsurface = myfont.render('H Ä V I S I T !!!', True, (100, 30, 30))
+    
     while True:
-        naytto.fill((0, 0, 0))
-        pygame.font.init()
-        myfont = pygame.font.SysFont('Comic Sans MS', 50)
-        textsurface = myfont.render('HÄVISIT !!!', False, (0, 0, 0))
-        naytto.blit(textsurface, (20, 100))
-        
-        pygame.display.flip()
+        naytto.fill(BLACK)        
+        naytto.blit(textsurface, (20, 100))    
 
         for tapahtuma in pygame.event.get():
             if tapahtuma.type == pygame.QUIT:
@@ -107,24 +122,20 @@ def lopetus():
             elif tapahtuma.type == pygame.KEYDOWN:
                 main()
 
+        pygame.display.flip()
+        kello.tick(60)
 
 
-x = 0
+
 y = KORKEUS - 100
-
-oikealle = False
-vasemmalle = False
-vauhti = 0.8
-jarrutan = -2
-vauhdinpudotus = 0
-askeleita = 0
-musa_paused = False
-
 jarrutusmatka = 30  # n. 20-60 sykliä, lasketaan KEYUP:ssa
 DANGER_TIME = 50
-danger_time = DANGER_TIME
 RED = (200, 0, 0)
+BLACK = (0, 0, 0)
+SEKUNTIMAARA = 30  # aikaa suorittaa peli
 
 kello = pygame.time.Clock()
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 50)
 
 main()
