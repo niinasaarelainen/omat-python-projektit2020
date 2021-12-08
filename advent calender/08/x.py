72578bdc97f994f92888b20c4312263d2d9fb6e1
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-12-08 18:19:51 +0200

    advent calender 8a

--- advent calender/08/x.py
+++ advent calender/08/x.py
@@ -1,57 +1,71 @@
-import math
+"""
 
-crabs = []
+0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+
+
+ 0: a- g, paitsi d  =6kpl   
+ 1: c, f            =2kpl
+ 2: a, c, d, e, g   =5kpl   
+ 3: a, c, d, f, g   =5kpl  
+ 4: b, c, d, f      =4kpl
+ 5: a, b, d, f, g   =5kpl    
+ 6: a- g, paitsi c  =6kpl   
+ 7: a, c, f         =3kpl
+ 8: a - g           =7kpl
+ 9: a- g, paitsi e  =6kpl   
+
+2 kirjainta : "1"   HELPOIN MUOTO
+3 kirjainta : "7"   2. HELPOIN MUOTO --> EI YLÄRIVIÄ = 4 (ykkönen jo selvitetty)
+4 kirjainta : "4"
+5 kirjainta : "2", "3", "5"
+6 kirjainta : "0", "6", "9"
+7 kirjainta : "8"
+
+"""
+
+alut_ja_loput = []
+output_values = []
+vastaus = 0
 
 
 def readfile():   # a-kohta
-    global crabs
+    global output_values
     f = open("data.txt", "r")         
     for rivi in f:
-        numerot_str = rivi.split(",")
-        for nro in numerot_str:
-            crabs.append(int(nro))
-    print(crabs)
+        alut_ja_loput.append(rivi.split(" | "))
+    print(alut_ja_loput)
+    for i in range(len(alut_ja_loput)):
+        output_values.append(alut_ja_loput[i][1].split(" "))
+    print()
+    print(output_values)
+    
+    
 
-
-def least_fuel():  # 1-part
-    mi = min(crabs)
-    ma = max(crabs)
-    fuel = 0
-    fuel_min = None
-
-    for point in range(mi, ma):
-        fuel = 0
-        for crab in crabs:
-            fuel += abs(crab - point)
-            
-        if fuel_min == None:
-            fuel_min = fuel
-        elif fuel < fuel_min:
-            fuel_min = fuel
-        print("fuel_min", fuel_min)
-
-
-def least_fuel2():   # b-part   92676646 oik.vast
-    mi = min(crabs)
-    ma = max(crabs)
-    fuel = 0
-    fuel_min = None
-
-    for point in range(mi, ma):
-        fuel = 0
-        for crab in crabs:
-            valimatka = abs(crab - point)
-            for i in range(valimatka + 1):
-                fuel += i
-            
-        if fuel_min == None:
-            fuel_min = fuel
-        elif fuel < fuel_min:
-            fuel_min = fuel
-
-    print("fuel_min", fuel_min)    
+def montako_1478():
+    global vastaus
+    for lista in output_values:
+        for str in lista:
+            if len(str.strip()) in [2, 3, 4, 7]:
+                vastaus += 1
+    print(vastaus)
 
 
 
 readfile()
-least_fuel2()
+montako_1478()
