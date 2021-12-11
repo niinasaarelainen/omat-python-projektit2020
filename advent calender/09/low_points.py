325b9aced4771831a66cfc4f5cfd85e8b98c263e
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-12-11 21:50:01 +0200

    advent calender 9a

--- advent calender/09/low_points.py
+++ advent calender/09/low_points.py
@@ -1,41 +1,62 @@
 import math
 
 data = []
-low_points = []
-points_lkm = 0
+low_points_data = []
+points = []
+risk_level = []
 
 
 def readfile():   # a-kohta
-    global crabs
     f = open("data_easy.txt", "r")         
     for rivi in f:
         data.append(rivi.strip())
     print(data)
 
-def kasittele_low_points():
-    global  points_lkm
+
+def kasittele_low_points_data():    # 585   too high !!!!
+    global  points
     for i in range(len(data)):
-        low_points.append([])
+        low_points_data.append([])
         for nro in data[i]:
-            low_points[i].append(nro)
+            low_points_data[i].append(int(nro))
 
-    low_point = low_points[0][0]
+    
+    for rivi in range(len(low_points_data)):  
+        for nro_ind in range(len(low_points_data[rivi])):             
+            if nro_ind < len(low_points_data[rivi]) -1: 
+                seuraava = int(low_points_data[rivi][nro_ind + 1] )    
+            else:
+                seuraava = 10
+            if nro_ind == 0:
+                edellinen = 10
+            else:
+                edellinen = int(low_points_data[rivi][nro_ind - 1])   
+            nro = int(low_points_data[rivi][nro_ind])
+            print(edellinen, nro, seuraava)
+            # eka rivi:   
+            if rivi == 0:
+                if nro < min(edellinen, seuraava) :                    
+                    points.append(nro)
 
-    for i in range(len(low_points)):
-        nro_muistiin = 0
-        for nro in low_points[i]:            
-                if i == 0:
-                    if nro < nro_muistiin:
-                        nro_muistiin = nro
-                        points_lkm += 1
-                elif i == len(low_points) - 1 :
-                    pass
-                else:
-                    if nro < nro_muistiin and nro < low_points[i -1][i] or nro < nro_muistiin and nro < low_points[i + 1][i]:
-                        nro_muistiin = nro
-                        points_lkm += 1
+            #vika rivi
+            elif rivi == len(low_points_data) - 1:
+                if nro < min(edellinen, seuraava, low_points_data[rivi -1][nro_ind]) :  
+                    points.append(nro)
+
+            #muut rivit
+            elif nro < (min(edellinen, seuraava, low_points_data[rivi + 1][nro_ind], low_points_data[rivi -1][nro_ind])):
+                #print("nro", nro,  "low_points_data[rivi + 1][nro_ind]", low_points_data[rivi + 1][nro_ind],  "low_points_data[rivi -1][nro_ind]", low_points_data[rivi -1][nro_ind])
+                points.append(nro)
+
+            print("points", points)
+
+
+def risk_level_count():
+    riskit= [i+1 for i in points]
+    print(sum(riskit))
 
 
 
 readfile()
-kasittele_low_points()
+kasittele_low_points_data()
+risk_level_count()
