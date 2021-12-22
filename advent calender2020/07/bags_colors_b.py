2e390876e075b3ce1ebcb65dc7b91042b658ed30
Niina Saarelainen <niinasaarelainen@gmail.com>
2021-12-22 21:12:08 +0200

    AdventOfCode 2020 7b for:lla

--- advent calender2020/07/bags_colors_b.py
+++ advent calender2020/07/bags_colors_b.py
@@ -2,6 +2,7 @@
 
 laukkujen_sisalto = {}
 polut = []
+laukkuja_yht = 0
 
 
 def readfile():   
@@ -10,29 +11,74 @@ def readfile():
         split1 = rivi.split(" bags contain ")
         laukkujen_sisalto[split1[0]] = split1[1].strip().split(", ")
 
-    print(laukkujen_sisalto)
+    #print(laukkujen_sisalto)
 
-
+"""
 def kay_lapi(solmu, polku): 
-  if "other bags" in solmu :  
-      return 
+    global laukkuja_yht
 
-  polku.append(solmu)
+    if "other bags" in solmu:
+        print('->'.join(polku))
+        polut.append(polku)
+        return 
+    
+    polku.append(solmu)
+    
+    for s in laukkujen_sisalto[solmu]:  # = seuraajat
+        uusi_polku = polku[:]
+        splitted = s.split(" ")
+        s = splitted[1] + " " + splitted[2]
+        if splitted[0] != "no":
+            laukkuja_yht += int(splitted[0])
+        print(laukkuja_yht)
+        kay_lapi(s, uusi_polku)  """
 
-  if "other bags" in solmu:
-      print('->'.join(polku))
-      polut.append(polku)
-      return 
-  
-  for s in laukkujen_sisalto[solmu]:  # = seuraajat
-      uusi_polku = polku[:]
-      splitted = s.split(" ")
-      s = splitted[1] + " " + splitted[2]
-      print(s)
-      kay_lapi(s, uusi_polku)
-  
+
+def kay_lapi(solmu, polku, lkm): 
+    global laukkuja_yht
+
+    print("lkm alussa: ",  lkm)
+
+    if "other bags" in solmu:
+        print('->'.join(polku))
+        laukkuja_yht += lkm
+        polut.append(polku)
+        return 
+    
+    polku.append(solmu)
+    
+    for s in laukkujen_sisalto[solmu]:  # = seuraajat
+        uusi_polku = polku[:]
+        splitted = s.split(" ")
+        s = splitted[1] + " " + splitted[2]
+        if splitted[0] != "no":
+            lkm *= int(splitted[0])
+            print(lkm, int(splitted[0]))
+        kay_lapi(s, uusi_polku, lkm)  
+        """
+        if splitted[0] != "no":
+            lkm = int(splitted[0]) """
 
  
+def muodosta_dict():
+    laukkujen_sisalto_key_on_value = {}
+    for l, laukut in laukkujen_sisalto.items():
+        total = 0
+        for laukku in laukut:        
+            splitted = laukku.split(" ")
+            laukku = splitted[1] + " " + splitted[2]
+            if splitted[0] != "no":
+                lkm = int(splitted[0])
+                total += lkm
+        laukkujen_sisalto_key_on_value[l] = total
+    print(laukkujen_sisalto_key_on_value)
+
+
+def etsi():
+    pass
+
 
 readfile()
-kay_lapi("shiny gold", [])
+#kay_lapi("shiny gold", [], 1)
+etsi()
+print(laukkuja_yht)
