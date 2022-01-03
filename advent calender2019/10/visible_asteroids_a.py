18a61dd49c06584cfa43b8b4207e551f42023edc
Niina Saarelainen <niinasaarelainen@gmail.com>
2022-01-03 18:03:26 +0200

    AdventOfCode 2019 10 a kesken2

--- advent calender2019/10/visible_asteroids_a.py
+++ advent calender2019/10/visible_asteroids_a.py
@@ -10,18 +10,19 @@ luvut = []
 def readfile():
     f = open("data_easy.txt", "r") 
     for rivi in f:
-        data.append(rivi.strip())
+        data.append([])
+        for merkki in rivi.strip():
+            data[-1].append(merkki.strip())
 
     print(data)
 
 
 def asteroidien_paikat():
-    global asteroidit
+    global data, asteroidit
     for rivi in range(len(data)):
         for merkki in range(len(data[0])):
             if data[rivi][merkki] == "#":
                 asteroidit.append([rivi, merkki])
-
 
 
 def tutki():
@@ -29,14 +30,20 @@ def tutki():
     laske_uudestaan = []
     for i in range(len(asteroidit)):
         asteroidit_copy = copy.deepcopy(asteroidit)
-        asteroidit_copy.pop(i)
-        for muut_asteroidit in asteroidit_copy:
-            if len(nakyy) == 0:
-                nakyy.append(muut_asteroidit)
-            else:
-                # APUA mitä jos lasketaan rivin vikaa.... niin ei voi mennä järjestyksessä
-                for a in nakyy:
-                    if 
+        tutkittava = asteroidit_copy.pop(i)
+        for muu_asteroidi in asteroidit_copy:
+            tutki_jaljella_olevat(tutkittava, muu_asteroidi)
+            asteroidit_copy.remove(muu_asteroidi)
+
+def tutki_jaljella_olevat(tutkittava, muu):
+    global data, asteroidit_copy
+    visible = 0
+    row = muu[0]
+    seat = muu[1]
+    tutkittava_row = tutkittava[0]
+    tutkittava_seat = tutkittava[1]
+    
+
 
 
 
@@ -44,3 +51,6 @@ def tutki():
 readfile()
 asteroidien_paikat()
 tutki()
+#tutki_8_suuntaa(0, 1)
+for rivi in data:
+    print(rivi)
