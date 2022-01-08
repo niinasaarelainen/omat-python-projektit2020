4f3c07bfc74a7feb0b8becf6b9de08eea524fc09
Niina Saarelainen <niinasaarelainen@gmail.com>
2022-01-08 19:46:14 +0200

    AdventOfCode 2018 13a carts_directions pygame toimii hidas

--- advent calender2018/13/cart_class.py
+++ advent calender2018/13/cart_class.py
@@ -1,30 +1,28 @@
 
 class Cart:
 
-    def __init__(self, x, y, myota):
+    def __init__(self, x, y, symboli):
         self.x = x
         self.y = y
-        self.myota = myota
+        self.symbolit = ["^", ">", "v", "<"]
+        self.symboli = symboli
         self.directions = [0, 1, 2, 3] # ylös, oik, alas, vas 
-        self.direction = self.directions[self.myota]
+        self.direction = self.directions[self.symbolit.index(self.symboli)]
         self.kaantosuunnat = [-1, 0, 1]    # left, straight, right
         self.kaanto_nro = 2
         self.kaantosuunta = self.kaantosuunnat[self.kaanto_nro]
-        self.symbolit = ["^", ">", "v", "<"]
-        self.symboli = self.symbolit[self.direction]
+        
         
     def next_direction(self):
         self.kaanto_nro += 1
         self.kaanto_nro = self.kaanto_nro % 3
         self.kaantosuunta = self.kaantosuunnat[self.kaanto_nro]
-        self.direction = self.direction + self.kaantosuunta
+        self.direction = (self.direction + self.kaantosuunta) % 4
         self.symboli = self.symbolit[self.direction]
 
-    def turn(self):
-        self.direction = (self.direction + self.myota)  % 4
+    def turn(self, suunta):
+        self.direction = (self.direction + suunta)  % 4
         self.symboli = self.symbolit[self.direction]
-
-
 
     def liiku(self):
         if self.direction == 0:
--- advent calender2018/13/temp.py
+++ advent calender2018/13/temp.py
@@ -1,1 +1,15 @@
-print(1 % 3)
+# liikuttelu vuorotellen, ongelma että tulee 3 x kaannos
+# kun cart on kaantoruudussa
+
+mika_cart = 0
+    while kesken:        
+        naytto.fill(BLACK)
+        for tapahtuma in pygame.event.get():
+            if tapahtuma.type == pygame.QUIT:
+                pygame.quit()  
+
+        c = carts[mika_cart]
+        c.liiku()
+        for c in carts:
+            piirra(c)
+        mika_cart = (mika_cart + 1) % len(carts)
