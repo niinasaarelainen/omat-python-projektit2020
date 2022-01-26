e1b4bf3a10c4faba4dbce745dbf3f748aa0611e0
Niina Saarelainen <niinasaarelainen@gmail.com>
2022-01-26 21:35:23 +0200

    adventof 2018 15a G_E_attack

--- advent calender2018/15/G_E_attack_a.py
+++ advent calender2018/15/G_E_attack_a.py
@@ -1,3 +1,4 @@
+import copy
 
 data = []
 e_x = 0
@@ -8,7 +9,7 @@ steps_all = {}
 
 
 def readfile():
-    f = open( "data_1.txt", "r") 
+    f = open( "data_3.txt", "r") 
     for rivi in f:
         rivi = rivi.strip()
         data.append([])
@@ -26,12 +27,17 @@ def find_E_and_G():
             elif data[r][m] == 'G':
                 gs.append([r, m])
 
+
+def loppuko(y, x):
+    if 'G' in [data[e_y -1][e_x], data[e_y +1][e_x], data[e_y][e_x -1], data[e_y][e_x +1]]:
+        print("   END")
+        return True
+
 def in_range():   # ?
     for g in gs:        
         g_y = g[0]
         g_x = g[1]
-        if 'G' in [data[e_y -1][e_x], data[e_y +1][e_x], data[e_y][e_x -1], data[e_y][e_x +1]]:
-            print("   END")
+        if loppuko(g_y, g_x):
             return "end"
 
         if data[g_y -1][g_x] == ".":
@@ -158,19 +164,76 @@ def liiku(y, x):
             e_x -= 1
     data[e_y][e_x] = "E"
 
-
-### MAIN ###################################
-
-readfile()
-
-while True:
-    find_E_and_G() 
-    if in_range() == "end":
-        break
     print()
     for rivi in data:
         print(rivi)
 
+    for g in gs:
+        if loppuko(g[0], g[1]):
+            return "end"
+
+def g_t_liikkuu():
+    global gs
+    global e_y, e_x
+    gs_copy = copy.deepcopy(gs)
+    gs = []
+    siirto_tehty = False
+
+    print("len(gs_copy)", len(gs_copy))
+    for g in gs_copy:
+        siirto_tehty = False
+        data[g[0]][g[1]] = "."
+        if g[1] == e_x:
+            if g[0] - e_y > 0 and data[g[0] - 1][g[1]] == ".":
+                data[g[0] - 1][g[1]] = "G"
+                gs.append([g[0] - 1, g[1]])
+                siirto_tehty = True
+            elif g[0] - e_y < 0 and data[g[0] + 1][g[1]] == ".":
+                data[g[0] + 1][g[1]] = "G"
+                gs.append([g[0] + 1, g[1]])
+                siirto_tehty = True
+        if siirto_tehty == False:
+            #print("siirto_tehty == False")
+            if g[1] - e_x > 0 and data[g[0]][g[1] -1] == ".":
+                data[g[0]][g[1] -1] = "G"
+                gs.append([g[0], g[1] - 1])
+                siirto_tehty = True
+            elif g[1] - e_x < 0 and data[g[0]][g[1] +1] == ".":
+                data[g[0]][g[1] +1] = "G"
+                gs.append([g[0], g[1] + 1])
+                siirto_tehty = True
+        if siirto_tehty == False:
+            if g[0] - e_y > 0 and data[g[0] - 1][g[1]] == ".":
+                data[g[0] - 1][g[1]] = "G"
+                gs.append([g[0] - 1, g[1]])   
+                siirto_tehty = True             
+            elif g[0] - e_y < 0 and data[g[0] + 1][g[1]] == ".":
+                data[g[0] + 1][g[1]] = "G"
+                gs.append([g[0] + 1, g[1]]) 
+                siirto_tehty = True
+        if siirto_tehty == False:
+            data[g[0]][g[1]] = "G" 
+
+    print()
+    for rivi in data:
+        print(rivi)
+
+    for g in gs:
+        if loppuko(g[0], g[1]):
+            return "end"
+        
+    
+
+### MAIN ###################################
+
+readfile()
+find_E_and_G() 
+
+#while True:
+for i in range(3):     
+    if in_range() == "end":
+        break
+    
     reachable()
     
     print(steps_all)
@@ -178,11 +241,11 @@ while True:
     y_x = st[0].split(",")
     print(y_x)
 
-    liiku(int(y_x[0]), int(y_x[1]))
-    print()
-    for rivi in data:
-        print(rivi)
-
-    g_t_liikkuu()   
+    if liiku(int(y_x[0]), int(y_x[1])) == "end":
+        break
+    if g_t_liikkuu() == "end":
+        break
+    
+    
 
 
