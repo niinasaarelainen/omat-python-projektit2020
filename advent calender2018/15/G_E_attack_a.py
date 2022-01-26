b27d12116ae4b1045c2451979cb3b834213a59da
Niina Saarelainen <niinasaarelainen@gmail.com>
2022-01-26 16:10:59 +0200

    adventof 2018 15a toimii puoliksi

--- advent calender2018/15/G_E_attack_a.py
+++ advent calender2018/15/G_E_attack_a.py
@@ -27,9 +27,13 @@ def find_E_and_G():
                 gs.append([r, m])
 
 def in_range():   # ?
-    for g in gs:
+    for g in gs:        
         g_y = g[0]
         g_x = g[1]
+        if 'G' in [data[e_y -1][e_x], data[e_y +1][e_x], data[e_y][e_x -1], data[e_y][e_x +1]]:
+            print("   END")
+            return "end"
+
         if data[g_y -1][g_x] == ".":
             data[g_y -1][g_x] = "?"
         if data[g_y +1][g_x] == ".":
@@ -40,8 +44,57 @@ def in_range():   # ?
             data[g_y][g_x +1] = "?"
 
 
-def reachable():   # @    
-    
+def diagonaalit(r, m):    # reachable alafunktio
+    steps = 0
+    ok = True
+    alkuloppu_x = sorted([e_x, m])
+    alkuloppu_y = sorted([e_y, r])
+    for x in range(alkuloppu_x[0] +1, alkuloppu_x[1] + 1):
+        if data[e_y][x] != ".":
+            ok = False
+            break
+        else:
+            steps += 1
+            #data[e_y][x] = "T" 
+    if ok:      
+        for y in range(alkuloppu_y[0] +1, alkuloppu_y[1]):
+            if data[y][m] != ".":
+                ok = False
+                break   
+            else:
+                steps += 1
+                #data[y][m] = "T" 
+    if ok:      
+        key = f"{r},{m}"
+        steps_all[key] = steps + 1
+        print(steps + 1)
+
+# diagonaaliin ensin alas   
+    ok = True
+    steps = 0
+    for y in range(alkuloppu_y[0] +1, alkuloppu_y[1] + 1):
+        if data[y][e_x] != ".":
+            ok = False
+            break   
+        else:
+            steps += 1
+            #data[y][e_x] = "T" 
+    if ok:
+        for x in range(alkuloppu_x[0] +1, alkuloppu_x[1]):
+            if data[r][x] != ".":
+                ok = False
+                break
+            else:
+                steps += 1
+                #data[r][x] = "T"  
+    if ok:   
+        key = f"{r},{m}"
+        if key not in steps_all:
+            steps_all[key] = steps + 1
+        print(steps + 1)
+
+
+def reachable():   # @   
     for r in range(len(data)):
         for m in range(len(data[r])):
             if data[r][m] == "?":
@@ -56,8 +109,7 @@ def reachable():   # @
                             break
                         else:
                             steps += 1
-                    if ok:        
-                        data[r][m] = "@" 
+                    if ok:  
                         key = f"{r},{m}"
                         steps_all[key] = steps + 1
 
@@ -71,61 +123,15 @@ def reachable():   # @
                         else:
                             steps += 1
                     if ok:        
-                        data[r][m] = "@" 
                         key = f"{r},{m}"
                         steps_all[key] = steps + 1
 
                 # diagonaaliin ensin suoraan     
                 else:
-                    alkuloppu_x = sorted([e_x, m])
-                    alkuloppu_y = sorted([e_y, r])
-                    for x in range(alkuloppu_x[0] +1, alkuloppu_x[1] + 1):
-                        print("r", r, "x", x)
-                        if data[e_y][x] != ".":
-                            ok = False
-                            break
-                        else:
-                            steps += 1
-                            print("step+1 x")
-                            data[e_y][x] = "T" 
-                    if ok:      
-                        for y in range(alkuloppu_y[0] +1, alkuloppu_y[1]):
-                            if data[y][m] != ".":
-                                ok = False
-                                break   
-                            else:
-                                steps += 1
-                                print("step+1 y")
-                                data[y][m] = "T" 
-                    if ok:        
-                        data[r][m] = "@" 
-                        key = f"{r},{m}"
-                        steps_all[key] = steps + 1
-                        print(steps + 1)
-
-                # diagonaaliin ensin alas   # TODO piirrä reitti testimielessä !!!!!!!
-                    for y in range(alkuloppu_y[0] +1, alkuloppu_y[1] + 1):
-                        if data[y][m] != ".":
-                            ok = False
-                            break   
-                        else:
-                            steps += 1
-                            print("step+1 y")
-                            data[y][m] = "T" 
-                    if ok:
-                        for x in range(alkuloppu_x[0] +1, alkuloppu_x[1]):
-                            if data[e_y][x] != ".":
-                                ok = False
-                                break
-                            else:
-                                steps += 1
-                                print("step+1 x")
-                                data[e_y][x] = "T"  
-                    if ok:        
-                        data[r][m] = "@" 
-                        key = f"{r},{m}"
-                        steps_all[key] = steps + 1
-                        print(steps + 1)
+                    diagonaalit(r, m)
+                    
+                data[r][m] = "."
+            
 
 
 
@@ -137,18 +143,46 @@ Targets:      In range:     Reachable:    Nearest:      Chosen:
 #.G.#G#       #?G?#G#       #@G@#G#       #!G.#G#       #.G.#G#
 #######       #######       #######       #######       #######     """
 
+def liiku(y, x):
+    global e_y, e_x
+    data[e_y][e_x] = "."
+    if x == e_x:
+        if y - e_y > 0:
+            e_y += 1
+        else:
+            e_y -= 1
+    else:
+        if x - e_x > 0:
+            e_x += 1
+        else:
+            e_x -= 1
+    data[e_y][e_x] = "E"
+
 
 ### MAIN ###################################
+
 readfile()
 
-find_E_and_G() 
-in_range()
-for rivi in data:
-    print(rivi)
+while True:
+    find_E_and_G() 
+    if in_range() == "end":
+        break
+    print()
+    for rivi in data:
+        print(rivi)
 
-reachable()
-print()
-for rivi in data:
-    print(rivi)
+    reachable()
+    
+    print(steps_all)
+    st = [k for k, v in sorted(steps_all.items(), key=lambda item: (item[1], item[0]))]
+    y_x = st[0].split(",")
+    print(y_x)
 
-print(steps_all)
+    liiku(int(y_x[0]), int(y_x[1]))
+    print()
+    for rivi in data:
+        print(rivi)
+
+    g_t_liikkuu()   
+
+
