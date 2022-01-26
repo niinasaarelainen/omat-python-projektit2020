84f3c9c01663dc161770c0dffb19878725b4e366
Niina Saarelainen <niinasaarelainen@gmail.com>
2022-01-26 14:49:04 +0200

    adventof 2018 15a työn alla 2

--- advent calender2018/15/G_E_attack_a.py
+++ advent calender2018/15/G_E_attack_a.py
@@ -3,6 +3,7 @@ data = []
 e_x = 0
 e_y = 0
 gs = []    # [y, x]
+steps_all = {}
 
 
 
@@ -38,30 +39,93 @@ def in_range():   # ?
         if data[g_y][g_x +1] == ".":
             data[g_y][g_x +1] = "?"
 
-def reachable():   # @
+
+def reachable():   # @    
+    
     for r in range(len(data)):
         for m in range(len(data[r])):
             if data[r][m] == "?":
+                steps = 0
                 #samalla rivillä:
-               
                 ok = True
                 if r == e_y:
-                    for x in range(e_x +1, m):
+                    alkuloppu = sorted([e_x, m])
+                    for x in range(alkuloppu[0] +1, alkuloppu[1]):                        
                         if data[r][x] != ".":
                             ok = False
                             break
-            if ok:        
-                    data[r][m] = "@" 
-                    
-                            
-
+                        else:
+                            steps += 1
+                    if ok:        
+                        data[r][m] = "@" 
+                        key = f"{r},{m}"
+                        steps_all[key] = steps + 1
 
                 #samassa sarakkeessa:
+                elif m == e_x:
+                    alkuloppu = sorted([e_y, r])
+                    for y in range(alkuloppu[0] +1, alkuloppu[1]):                        
+                        if data[y][e_x] != ".":                            
+                            ok = False
+                            break
+                        else:
+                            steps += 1
+                    if ok:        
+                        data[r][m] = "@" 
+                        key = f"{r},{m}"
+                        steps_all[key] = steps + 1
 
+                # diagonaaliin ensin suoraan     
+                else:
+                    alkuloppu_x = sorted([e_x, m])
+                    alkuloppu_y = sorted([e_y, r])
+                    for x in range(alkuloppu_x[0] +1, alkuloppu_x[1] + 1):
+                        print("r", r, "x", x)
+                        if data[e_y][x] != ".":
+                            ok = False
+                            break
+                        else:
+                            steps += 1
+                            print("step+1 x")
+                            data[e_y][x] = "T" 
+                    if ok:      
+                        for y in range(alkuloppu_y[0] +1, alkuloppu_y[1]):
+                            if data[y][m] != ".":
+                                ok = False
+                                break   
+                            else:
+                                steps += 1
+                                print("step+1 y")
+                                data[y][m] = "T" 
+                    if ok:        
+                        data[r][m] = "@" 
+                        key = f"{r},{m}"
+                        steps_all[key] = steps + 1
+                        print(steps + 1)
 
-                # diagonaaliin ensin suoraan
-
-                # diagonaaliin ensin alas
+                # diagonaaliin ensin alas   # TODO piirrä reitti testimielessä !!!!!!!
+                    for y in range(alkuloppu_y[0] +1, alkuloppu_y[1] + 1):
+                        if data[y][m] != ".":
+                            ok = False
+                            break   
+                        else:
+                            steps += 1
+                            print("step+1 y")
+                            data[y][m] = "T" 
+                    if ok:
+                        for x in range(alkuloppu_x[0] +1, alkuloppu_x[1]):
+                            if data[e_y][x] != ".":
+                                ok = False
+                                break
+                            else:
+                                steps += 1
+                                print("step+1 x")
+                                data[e_y][x] = "T"  
+                    if ok:        
+                        data[r][m] = "@" 
+                        key = f"{r},{m}"
+                        steps_all[key] = steps + 1
+                        print(steps + 1)
 
 
 
@@ -77,7 +141,6 @@ Targets:      In range:     Reachable:    Nearest:      Chosen:
 ### MAIN ###################################
 readfile()
 
-
 find_E_and_G() 
 in_range()
 for rivi in data:
@@ -87,3 +150,5 @@ reachable()
 print()
 for rivi in data:
     print(rivi)
+
+print(steps_all)
