39f10d1a931c01be5736b3b1dc46a58bd418e097
Niina Saarelainen <niinasaarelainen@gmail.com>
2022-12-19 12:12:45 +0200

    2022,2017

--- advent calender2022/08/visible_b.py
+++ advent calender2022/08/visible_b.py
@@ -3,7 +3,7 @@ visible_all = []
 
 def readfile():   # a-kohta
     global visible
-    f = open("data_a.txt", "r") # a-kohta !!!!      
+    f = open("data.txt", "r") # a-kohta !!!!      
     for rivi in f:
         data.append(rivi.strip()) 
 
@@ -20,38 +20,57 @@ def kasittele():
                 visible_o = 0
                 visible_y = 0
                 visible_a = 0
+                ei_enaa = False
                 if i == 0 or i == len(data[rivi]) - 1:  #reunat
                     pass
                 else:
+                    #print("\n rivi, i:", rivi, i)
                     # vasemmalla
-                    for ch in data[rivi][:i]:  
-                        print("ch , data[rivi][:i]", ch ,  data[rivi][:i])                      
-                        if ch <= data[rivi][i]:
-                            visible_v += 1                              
+                    for ch in reversed(data[rivi][:i]):  
+                        #print("ch , data[rivi][:i]", ch ,  data[rivi][:i])                      
+                        if ch < data[rivi][i] and ei_enaa == False:
+                            visible_v += 1    
+                        elif ei_enaa == False:
+                            visible_v += 1                            
+                            ei_enaa = True                          
                         
-                    # oikealla"    # array  reversed !?!?!?!?!?!!?
+                    # oikealla"    
+                    ei_enaa = False
                     for ch in data[rivi][(i+1):]:
-                        #print("ch , data[rivi][i+1:]", ch ,  data[rivi][(i+1):])
-                        if ch <= data[rivi][i]:
-                            visible_o += 1              
+                        #print("oike", ch)
+                        if ch < data[rivi][i] and ei_enaa == False:
+                            visible_o += 1  
+                        elif ei_enaa == False:
+                            visible_o += 1 
+                            ei_enaa = True               
                         
                     #ylh
+                    ei_enaa = False
                     s = muodosta_sarake(i)
-                    for ch in s[:rivi]:
-                        if ch <= s[rivi]:
-                            visible_y += 1               
+                    for ch in reversed(s[:rivi]):
+                        if ch < s[rivi] and ei_enaa == False:
+                            visible_y += 1   
+                        elif ei_enaa == False:
+                            visible_y += 1 
+                            ei_enaa = True              
                         
                     #alh
+                    ei_enaa = False
                     s = muodosta_sarake(i)
                     for ch in s[rivi + 1:]:
-                        if ch <= s[rivi]:
-                            visible_a += 1   
+                        
+                        if ch < s[rivi] and ei_enaa == False:
+                            visible_a += 1  
+                        elif ei_enaa == False:
+                            visible_a += 1 
+                            ei_enaa = True   
                    
 
-                    print(visible_v, visible_o , visible_y , visible_a)
-                    tulo = visible_a * visible_o * visible_v * visible_y
-                    print("tulo, rivi, i: ", tulo, rivi, i)
-                    visible_all.append(visible)
+                    
+                    #print(visible_v, visible_o , visible_y , visible_a)
+                    tulo = visible_a  * visible_o * visible_v * visible_y
+                    #print("tulo, rivi, i: ", tulo, rivi, i)
+                    visible_all.append(tulo)
 
 def muodosta_sarake(i):
     return [rivi[i] for rivi in data]
@@ -59,4 +78,4 @@ def muodosta_sarake(i):
 
 readfile()
 kasittele() 
-print(visible)      # 1676      
+print(max(visible_all))     
