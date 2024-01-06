e988ea2ba3a8ea5e4b7024572a545a068d619e60
Niina Saarelainen <niinasaarelainen@gmail.com>
2024-01-06 10:12:52 +0200

    print paths

--- advent calender2022/12/printPaths_orig.py
+++ advent calender2022/12/printPaths_orig.py
@@ -1,34 +1,22 @@
-
+data = []
 m = []
 loppuu_keskelle = []
+end = ord("E")
 
 def readfile():  
-    f = open("data.txt", "r")         
+    f = open("data_1.txt", "r")         
     for rivi in f:
-        rivi = rivi.strip()
+        data.append(rivi.strip())
+
+
+def muunnaNumeroiksi():
+    for rivi in data:
         r = []
-        for luku in rivi:
-            r.append(int(luku))
+        for kirjain in rivi:
+            r.append(ord(kirjain))
         m.append(r)
+        r = []
 
-
-def print_paths(row=0, col=0, s=""):
-    
-    #this is the recursive base case, if we're at the bottom right corner after this traversal, 
-    #we're done with this path.
-    if(row == (len(m) - 1) and col == (len(m[0]) - 1)): #I hard coded the index pair into the function, but this can be changed
-
-        s += "->" + str(m[row][col]) #concatenate the final node onto the running total
-        #print(s)
-        return
-        
-    #If we overshot, no big deal, just don't count it among the solutions and return without printing.
-    if(row > (len(m) - 1) or col > (len(m[0]) - 1)):
-        return
-        
-    #recursively search each node below and to the right, and concatenate the current node to the running total.
-    print_paths(row, col+1, s + "->" + str(m[row][col]))
-    print_paths(row+1, col, s + "->" + str(m[row][col]))
 
 
 def print_paths_oma(row=0, col=0, s=""):    
@@ -39,7 +27,7 @@ def print_paths_oma(row=0, col=0, s=""):
     #If we overshot, no big deal, just don't count it among the solutions and return without printing.
     if(row > (len(m) - 1) or row < 0 or  col > (len(m[0]) - 1) or col < 0):
         return
-    elif (m[row][col] == 8):
+    elif (m[row][col] == end):
             s += "->" + str(m[row][col]) #concatenate the final node onto the running total
             loppuu_keskelle.append(s)
             return
@@ -55,8 +43,14 @@ def print_paths_oma(row=0, col=0, s=""):
 
 
 readfile()
+muunnaNumeroiksi()
+print(m)
 print_paths_oma()  
 print(len(loppuu_keskelle))
+
 for s in loppuu_keskelle:
-    print(s)
+    for merkki in range(len(s)-2):
+        if (int(s[merkki]) - int(s[merkki +1]) != -1):    # TODO
+            continue
+        print(s)
 print()
