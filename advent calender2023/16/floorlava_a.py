0e26385621f5b62660a9423e04cad2499174d6fb
Niina Saarelainen <niinasaarelainen@gmail.com>
2024-10-21 15:23:36 +0300

    AOC2023 16 tyon alla taas2

--- advent calender2023/16/floorlava_a.py
+++ advent calender2023/16/floorlava_a.py
@@ -24,12 +24,14 @@ class Beam:
         #print(" kaanna:",merkki, self.y, self.x, self.suunta)     
 
         if '\\' == merkki: 
+            #print("  ehp", self.y, self.x, self.suunta)
             if self.suunta == "oik":
                 self.suunta = "alas"                
             elif self.suunta == "alas":
                 self.suunta = "oik"
             elif self.suunta == "vas":
                 self.suunta = "ylos"
+                
             elif self.suunta == "ylos":                
                 self.suunta = "vas"
 
@@ -44,85 +46,92 @@ class Beam:
                 self.suunta = "oik"
 
         if '|' == merkki:     
-            self.edellinensuunta = self.suunta           
-            self.suunta = "alas"
+            self.edellinensuunta = self.suunta 
+            if self.suunta != "ylos":   # ylösmennessä jatkaa samaan suuntaan                
+               self.suunta = "alas"
 
         if '-' == merkki:      
             self.edellinensuunta = self.suunta                  
-            self.suunta = "oik"
+            if self.suunta != "vas":    # vas mennessä jatkaa samaan suuntaan    
+                self.suunta = "oik"
 
     def __repr__(self) -> str:
-        return f'{self.id}, {self.suunta}, {self.y}, {self.x}'
+        return f'{self.id}, {self.suunta}, ({self.y}, {self.x})'
 
 
 data = []
+data_orig = []
 beamit = []
 uudet_beamit = []
 id = 0
 
 def readfile():   
-    global y_max, x_max
-    f = open("data_1.txt", "r")         
+    global y_max, x_max, data_orig, data
+    f = open("data.txt", "r")         
     for rivi in f:           
         rivi = rivi.strip()
         taul = []
         for merkki in rivi:
             taul.append(merkki)
         data.append(taul)   
+
     y_max = len(data)
     x_max = len(data[0])
+    data_orig = copy.deepcopy(data)
     
 
 def lue():
     global beamit, uudet_beamit
     uudet_beamit = copy.deepcopy(beamit)
+    beamit = []
     print(len(uudet_beamit))
     for beam in uudet_beamit:
         beam.tuhoaMinut = False
         while True:  
-            #print("alussa:", beam)    # TODO mistä miinusmerkkiset sijainnit tulevat !?!?!?!?!?
             if beam.x >= 0 and beam.y >= 0 and beam.x < x_max and beam.y < y_max :  
-                uudet(data[beam.y][beam.x], beam.edellinensuunta, beam.y, beam.x)       # edellinensuunta  !!!!!
+                uudet(data_orig[beam.y][beam.x], beam.edellinensuunta, beam.y, beam.x)       # edellinensuunta  !!!!!
                 data[beam.y][beam.x] = '#'
             beam.liiku() 
             if beam.x >= 0 and beam.y >= 0 and beam.x < x_max and beam.y < y_max :  
-                beam.kaanna(data[beam.y][beam.x])                
+                beam.kaanna(data_orig[beam.y][beam.x])                
             else:
-                beam.tuhoaMinut = True   # tarvitaanko
-                break
-            #print("lopussa:", beam)
-        if not beam.tuhoaMinut:
-            beamit.append(beam)
-        print(beam)
+                beam.tuhoaMinut = True  
+                break        
 
 
 def uudet(merkki, suunta, y, x):
     global beamit, id
-    if '|' == merkki:   
-        #print("@uudet: |", suunta)             
+    if '|' == merkki:        
         if suunta == "oik" or suunta == "vas" : 
             id += 1
-            beamit.append(Beam("ylos", y, x, id))  
+            if y > 0:
+                beamit.append(Beam("ylos", y, x, id))  
+                #print(beamit)
 
-    if '-' == merkki:   
-        print("@uudet: -", suunta)                    
+    if '-' == merkki:                 
         if suunta == "ylos" or suunta == "alas" : 
             id += 1
-            beamit.append(Beam("vas", y, x, id))  
+            if x > 0:
+                beamit.append(Beam("vas", y, x, id))  
+                #print(beamit)
 
 
 def piirra():
     print()
-    for rivi in range(len(data)): 
-        for sarake in range(len(data[rivi])):   
+    montako = 0
+    for rivi in range(len(data_orig)): 
+        for sarake in range(len(data_orig[rivi])):   
             print(data[rivi][sarake], end="")
-        print()
+            if data[rivi][sarake] == '#':
+                montako += 1
+    print(montako)
   
 
 readfile()  
 
 beamit.append(Beam("oik", 0, 0, id))
 #while beamit != []:
-for i in range(5):
+for i in range(18):
     lue()
-    piirra()
+    
+piirra()
