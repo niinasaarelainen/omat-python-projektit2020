418477d2d3fa84a4620ef2ac51e5e675ffca87e1
Niina Saarelainen <niinasaarelainen@gmail.com>
2024-10-26 18:12:00 +0300

    AOC2021 21

--- advent calender2021/21/die_a.py
+++ advent calender2021/21/die_a.py
@@ -1,42 +1,47 @@
 
 positions = [4, 8]     # 4-->7 !!!
+positions = [7, 8]
 die_roll_count = 0
 scores = [0, 0]
 pawn1 = 0
 pawn2 = 0
 menossa_lukema = 1
+vastaus = 0
 
-def heita():
-    global menossa_lukema, score1, score2, die_roll_count, positions
+def heita(kumman_vuoro):
+    global menossa_lukema, die_roll_count, vastaus       
+    valitulos = 0        
+    for i in range(3):
+        die_roll_count += 1
+        
+        valitulos += menossa_lukema
+        menossa_lukema += 1
+        if menossa_lukema > 100:
+            menossa_lukema = 1
     
-    for kumman_vuoro in range(2):
-        valitulos = 0
+    print("valitulos", valitulos)
+    positions[kumman_vuoro % 2] += valitulos - 1
+    positions[kumman_vuoro % 2] = positions[kumman_vuoro % 2] % 10 + 1
+    scores[kumman_vuoro % 2] += positions[kumman_vuoro % 2]
+    if max(scores) >= 1000:
+        print("    hep")
+        vastaus = die_roll_count
+        print("vastaus", vastaus)
         
-        for i in range(3):
-            die_roll_count += 1
-            if die_roll_count >= 1000:
-                print("moi")        # TODO  !?!?!?!!??!?!
-                return die_roll_count
-            valitulos += menossa_lukema
-            menossa_lukema += 1
-            if menossa_lukema > 100:
-                menossa_lukema = 1
-        
-        print("valitulos", valitulos)
-        positions[kumman_vuoro % 2] += valitulos - 1
-        positions[kumman_vuoro % 2] = positions[kumman_vuoro % 2] % 10 + 1
-        scores[kumman_vuoro % 2] += positions[kumman_vuoro % 2]
-        
-    print(scores)
+    print("scores", scores)
     print("positions", positions)
-    return die_roll_count
+    return vastaus
 
 
 
+kumman_vuoro = 0
+while vastaus == 0 :      # 1000
+    print("kumman_vuoro", kumman_vuoro, "vastaus", vastaus)
+    vastaus = heita(kumman_vuoro)
+    print("vastaus", vastaus)
+    kumman_vuoro += 1
+print("vastaus", vastaus)
 
-while max(scores) < 1000:
-    count = heita()
-print(count)
-
-print(scores)
+print("scores", scores)
 print(die_roll_count)
+print(vastaus * min(scores))
