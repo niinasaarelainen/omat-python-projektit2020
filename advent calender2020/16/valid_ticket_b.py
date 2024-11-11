15f87e17aa5fbc5cbb4fa70264ccb5c6e9965752
Niina Saarelainen <niinasaarelainen@gmail.com>
2024-11-11 09:13:45 +0200

    AOC2020 16b

--- advent calender2020/16/valid_ticket_b.py
+++ advent calender2020/16/valid_ticket_b.py
@@ -1,52 +1,102 @@
 data = []
-ranget = []   # [['1', '3'], ['5', '7'], ['6', '11'], ['33', '44'], ['13', '40'], ['45', '50']]
-tickets = []   # ['7', '3', '47', '40', '4', '50', '55', '2', '20', '38', '6', '12']
-your_ticket = [107,157,197,181,71,113,179,109,97,163,73,53,101,193,173,151,167,191,127,103]
+ranget = {}  
+tickets = []  
+tickets_sarakkeet = []  
+tickets_sarakkeet_hash = {}
+your_ticket = {11 : "" , 12 : "" , 13 : "" }  # "" tulee rangen nimi myöhemmin
+#your_ticket = [107,157,197,181,71,113,179,109,97,163,73,53,101,193,173,151,167,191,127,103]
 
 
 def readfile():  
-    f = open("data_1_ranget.txt", "r")  
+    f = open("data_b_ranget.txt", "r")  
 
     for rivi in f:
-        ind = rivi.find(":")
-        print(ind)
-        rivi = rivi[ind:]
+        nimi, rivi = rivi.split(":")
         sp = rivi.strip().replace(" or", "").split(" ")
         for osa in sp:
             if ":" not in osa:
                 min, max = osa.split("-")
-                ranget.append([int(min), int(max)])
+                if nimi not in ranget:
+                    ranget[nimi] = [[int(min), int(max)]]
+                else:
+                    ranget[nimi].append([int(min), int(max)])
+        ranget[nimi].append([])
 
     print(ranget)
 
-    f = open("data_1_tickets.txt", "r")  
+    f = open("data_b_tickets.txt", "r")  
 
     for rivi in f:
         sp = rivi.strip().split(",")
         ticket = []
         for luku in sp:
             ticket.append(int(luku))
-        tickets.append(ticket)
+        tickets.append(ticket)    
 
-    print(tickets)
-                    
+
+def muodosta_tickets_sarakkeet():       
+    for ind in range(len(ranget)):
+        tickets_sarakkeet.append([])
+        for t in tickets:
+            tickets_sarakkeet[-1].append(t[ind])       
+    #print(tickets_sarakkeet)                            #  [3, 15, 5], [9, 1, 14], [18, 5, 9]
+
+def muodosta_tickets_sarakkeet_hash():
+    for s in tickets_sarakkeet:
+        print(" s", s)
+        tickets_sarakkeet_hash[str(s)] = []
+    
+    print(tickets_sarakkeet_hash)
 
 
 def onkoRangella():
-    validit = []  
+    validit_luvut = []  
 
     for t in tickets:
         for luku in t:
-            for min, max in ranget:
+            for min, max in ranget.values():
                 if luku >= min and luku <= max:
-                    print(luku, "on välillä", min, max)
-                    validit.append(luku) 
+                    #print(luku, "on välillä", min, max)
+                    validit_luvut.append(luku) 
                     break
-        print("validit", validit)
 
-        epävalidit = [luku for luku in tickets if luku not in validit]
-        return epävalidit
+    epävalidit = [t for t in tickets for luku in t if luku not in validit_luvut]
+    validit = [t for t in tickets if t not in epävalidit]
+    return validit
+
+
+def positions():
+    ind = 1
+    for t in tickets:   # [3, 9, 18]
+        for luku in t:
+            for nimi, values in ranget.items():
+                
+                for i in range(0, 2):
+                    min, max = values[i]
+                    if luku >= min and luku <= max:
+                        ranget[nimi][-1].append(luku)
+
+def loytyyko_rangeista():
+    for nimi, v in ranget.items():
+        for sarake in tickets_sarakkeet:
+            if len(set(v[-1]) & set(sarake)) == len(ranget):                              # set ja &   !!!!!!!
+                tickets_sarakkeet_hash[str(sarake)].append(nimi)
+
+
+def tutki_hashit():
+    s =sorted(tickets_sarakkeet_hash, key=lambda h: len(tickets_sarakkeet_hash[h]))
+    print(tickets_sarakkeet_hash)
+
+    #s = sorted(tulokset, key=lambda tulos: (-tulos.topit_lkm, tulos.topit_yritykset, -tulos.zonet_lkm, tulos.zonet_yritykset))  
+      
+        
+                        
 
 
 readfile()
-print(onkoRangella())
+muodosta_tickets_sarakkeet()
+muodosta_tickets_sarakkeet_hash()
+positions()
+print(ranget)
+loytyyko_rangeista()
+tutki_hashit()
